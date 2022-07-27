3c07c322d69fd8083e67444d2d6b77467a992cc8
cutajarj <james.cutajar@gmail.com>
2022-07-27 09:13:57 +0100

    Adding sequential listing

---
+++ chapter2/listing2.2/sequential.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doWork(id int) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(id, "Work finished")
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		doWork(i)
+	}
+}
