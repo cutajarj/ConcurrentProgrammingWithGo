31ec16c481a8483d23b7eb36e9ceb3a888084658
cutajarj <james.cutajar@gmail.com>
2022-07-29 08:15:04 +0100

    Adding parallel listing

---
+++ chapter2/listing2.3/parallel.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func doWork(id int) {
+    time.Sleep(1 * time.Second)
+    fmt.Println(id, "Work finished")
+}
+
+func main() {
+    for i := 0; i < 5; i++ {
+        go doWork(i)
+    }
+    time.Sleep(2 * time.Second)
+}
