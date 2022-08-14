4ecaf3c2941cf18b535c5694589cb26463000513
cutajarj <james.cutajar@gmail.com>
2022-08-14 07:25:45 +0100

    Adding match monitor with normal mutex

---
+++ chapter4/listing4.5/matchmonitor.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "container/list"
+    "fmt"
+    "strconv"
+    "sync"
+    "time"
+)
+
+func matchRecorder(matchEvents *list.List, mutex *sync.Mutex) {
+    for i := 0; ; i++ {
+        mutex.Lock()
+        matchEvents.PushBack("Match event " + strconv.Itoa(i))
+        mutex.Unlock()
+        time.Sleep(1 * time.Second)
+        fmt.Println("Appended match event")
+    }
+}
+
+func clientHandler(mEvents *list.List, mutex *sync.Mutex, st time.Time) {
+    mutex.Lock()
+    allEvents := copyAllEvents(mEvents)
+    mutex.Unlock()
+    timeTaken := time.Since(st)
+    fmt.Println(len(allEvents), "events copied in", timeTaken)
+}
+
+func copyAllEvents(matchEvents *list.List) []string {
+    i := 0
+    allEvents := make([]string, matchEvents.Len())
+    for e := matchEvents.Front(); e != nil; e = e.Next() {
+        allEvents[i] = e.Value.(string)
+        i++
+    }
+    return allEvents
+}
+
+func main() {
+    mutex := sync.Mutex{}
+    var matchEvents = list.New()
+    for j := 0; j < 10000; j++ {
+        matchEvents.PushBack("Match event")
+    }
+    go matchRecorder(matchEvents, &mutex)
+    start := time.Now()
+    for j := 0; j < 50000; j++ {
+        go clientHandler(matchEvents, &mutex, start)
+    }
+    time.Sleep(100 * time.Second)
+}
