e9c0c185b513b85466e1614ada9b1e0b24fabf0a
cutajarj <james.cutajar@gmail.com>
2022-08-19 08:37:56 +0100

    Adding exercise 1

---
+++ exercises/chapter2/exercise2.1/catfiles.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+"fmt"
+    "log"
+    "os"
+    "time"
+)
+
+/*
+  You can run this by executing:
+  go run catfiles.go ../../commonfiles/txtfile1 ../../commonfiles/txtfile2 ../../commonfiles/txtfile3
+ */
+
+func printFile(filename string) {
+    content, err := os.ReadFile(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(string(content))
+}
+
+func main() {
+    filenames := os.Args[1:]
+    for _, filename := range filenames {
+        go printFile(filename)
+    }
+    time.Sleep(2 * time.Second)
+}
+
