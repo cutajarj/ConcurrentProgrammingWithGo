b30b1b4328c4710dcfa8ece4337196a977a442d1
cutajarj <james.cutajar@gmail.com>
2022-09-01 09:20:00 +0100

    Adding exercises

---
+++ exercises/chapter2/exercise2.2/grepfiles.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "log"
+    "os"
+    "strings"
+    "time"
+)
+
+/*
+  You can run this by executing:
+  go run grepfiles.go Mozilla ../../commonfiles/txtfile1 ../../commonfiles/txtfile2 ../../commonfiles/txtfile3
+ */
+
+func grepFile(filename string, searchStr string) {
+    content, err := os.ReadFile(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if strings.Contains(string(content), searchStr) {
+        println(filename, "contains a match with", searchStr)
+    } else {
+        println(filename, "does NOT contain a match with", searchStr)
+    }
+}
+
+func main() {
+    searchStr := os.Args[1]
+    filenames := os.Args[2:]
+    for _, filename := range filenames {
+        go grepFile(filename, searchStr)
+    }
+    time.Sleep(2 * time.Second)
+}
+
