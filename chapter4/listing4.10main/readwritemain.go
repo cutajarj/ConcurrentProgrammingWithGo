ec830bd4618b911b3579477422dd99cf5c07e9e8
cutajarj <james.cutajar@gmail.com>
2022-09-06 07:35:19 +0100

    read write mutex, extracting main method

---
+++ chapter4/listing4.10main/readwritemain.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.10"
+    "time"
+)
+
+func main() {
+    rwMutex := listing4_10.ReadWriteMutex{}
+    for i := 0; i < 10; i++ {
+        go func() {
+            rwMutex.ReadLock()
+            println("Read started")
+            time.Sleep(5 * time.Second)
+            println("Read done")
+            rwMutex.ReadUnlock()
+        }()
+    }
+    time.Sleep(1 * time.Second)
+    println("Write started")
+    rwMutex.WriteLock()
+    println("Write finished")
+}
+
