74e79e5b277612ed608a8223ab66896003547749
cutajarj <james.cutajar@gmail.com>
2022-09-07 16:12:24 +0100

    chapter 5 negative spending

---
+++ chapter5/listing5.1/stingyspendynegative.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "os"
+    "sync"
+    "time"
+)
+
+func stingy(money *int, mutex *sync.Mutex) {
+    for i := 0; i < 1000000; i++ {
+        mutex.Lock()
+        *money += 10
+        mutex.Unlock()
+    }
+    println("Stingy Done")
+}
+
+func spendy(money *int, mutex *sync.Mutex) {
+    for i := 0; i < 200000; i++ {
+        mutex.Lock()
+        *money -= 50
+        if *money < 0 {
+            println("Money is negative!")
+            os.Exit(1)
+        }
+        mutex.Unlock()
+    }
+    println("Spendy Done")
+}
+
+func main() {
+    money := 100
+    mutex := sync.Mutex{}
+    go stingy(&money, &mutex)
+    go spendy(&money, &mutex)
+    time.Sleep(2 * time.Second)
+    mutex.Lock()
+    println("Money in bank account: ", money)
+    mutex.Unlock()
+}
