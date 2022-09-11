41c87e08b814311b635bc64da6b0259f42def4c4
cutajarj <james.cutajar@gmail.com>
2022-09-11 07:57:15 +0100

    chapter 3 exercise

---
+++ exercises/chapter3/exercise3.3/waitfornextnum.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+/*
+  Note: this program has a race condition for demonstration purposes
+*/
+
+func addNextNumber(nextNum *[101]int) {
+    i := 0
+    for nextNum[i] != 0 {
+        i++
+    }
+    nextNum[i] = nextNum[i-1] + 1
+}
+
+func main() {
+    nextNum := [101]int{1}
+    for i := 0; i < 100; i++ {
+        go addNextNumber(&nextNum)
+    }
+    for nextNum[100] == 0 {
+        println("Waiting for goroutines to complete")
+        time.Sleep(10 * time.Millisecond)
+    }
+    fmt.Println(nextNum)
+}
