325c332c336519cf4daa0b3a4589f93331a777d8
cutajarj <james.cutajar@gmail.com>
2022-09-15 07:31:29 +0100

    refactoring and leaving only main in listing 4.2

---
+++ chapter4/listing4.2/charcountermutexslowmain.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.3"
+    "sync"
+    "time"
+)
+
+func main() {
+    mutex := sync.Mutex{}
+    var frequency = make([]int, 26)
+    for i := 1000; i <= 1200; i++ {
+        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+        go listing4_3.CountLetters(url, frequency, &mutex)
+    }
+    time.Sleep(10 * time.Second)
+    mutex.Lock()
+    fmt.Println(frequency)
+    mutex.Unlock()
+}
