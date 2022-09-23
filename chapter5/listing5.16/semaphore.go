674de6847af129fed2c135f3d5a6cd75e5b820ae
cutajarj <james.cutajar@gmail.com>
2022-09-23 10:25:58 +0100

    Adding semaphore

---
+++ chapter5/listing5.16/semaphore.go
@@ -0,0 +1,33 @@
+package listing5_16
+
+import (
+    "sync"
+)
+
+type Semaphore struct {
+    permits int
+    cond *sync.Cond
+}
+
+func NewSemaphore(n int) *Semaphore {
+    return &Semaphore{
+        permits: n,
+        cond: sync.NewCond(&sync.Mutex{}),
+    }
+}
+
+func (rw *Semaphore) Acquire() {
+    rw.cond.L.Lock()
+    for rw.permits <= 0 {
+        rw.cond.Wait()
+    }
+    rw.permits--
+    rw.cond.L.Unlock()
+}
+
+func (rw *Semaphore) Release() {
+    rw.cond.L.Lock()
+    rw.permits++
+    rw.cond.Signal()
+    rw.cond.L.Unlock()
+}
