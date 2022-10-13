c4251f4aba3ed8c484f972c82dc37cdbc5f0c5b8
cutajarj <james.cutajar@gmail.com>
2022-10-13 10:40:12 +0100

    solution to exercise 4.2

---
+++ exercises/chapter4/exercise4.2/readtrywritemutex.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+"sync"
+)
+
+type ReadWriteMutex struct {
+    readersCounter int
+    readersLock    sync.Mutex
+    globalLock     sync.Mutex
+}
+
+func (rw *ReadWriteMutex) ReadLock() {
+    rw.readersLock.Lock()
+    rw.readersCounter++
+    if rw.readersCounter == 1 {
+        rw.globalLock.Lock()
+    }
+    rw.readersLock.Unlock()
+}
+
+func (rw *ReadWriteMutex) WriteLock() {
+    rw.globalLock.Lock()
+}
+
+func (rw *ReadWriteMutex) TryWriteLock() bool {
+    return rw.globalLock.TryLock()
+}
+
+func (rw *ReadWriteMutex) ReadUnlock() {
+    rw.readersLock.Lock()
+    rw.readersCounter--
+    if rw.readersCounter == 0 {
+        rw.globalLock.Unlock()
+    }
+    rw.readersLock.Unlock()
+}
+
+func (rw *ReadWriteMutex) WriteUnlock() {
+    rw.globalLock.Unlock()
+}
+
+
