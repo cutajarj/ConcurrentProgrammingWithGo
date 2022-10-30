5b3316bbab14c38edda6149dbb3d14008903be82
cutajarj <james.cutajar@gmail.com>
2022-10-30 06:25:14 +0000

    Directional sender and receiver

---
+++ chapter7/listing7.8/directional.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func receiver(messages <-chan int) {
+    for {
+        msg := <-messages
+        fmt.Println(time.Now().Format("15:04:05"), "Received:", msg)
+    }
+}
+
+func sender(messages chan<- int) {
+    for i := 1; ; i++ {
+        fmt.Println(time.Now().Format("15:04:05"), "Sending:", i)
+        messages <- i
+        time.Sleep(1 * time.Second)
+    }
+}
+
+func main() {
+    msgChannel := make(chan int)
+    go receiver(msgChannel)
+    go sender(msgChannel)
+    time.Sleep(5 * time.Second)
+}
