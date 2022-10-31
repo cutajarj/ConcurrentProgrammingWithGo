d5a247401265d09ef68d6c71e05561fc1cbba23d
cutajarj <james.cutajar@gmail.com>
2022-10-31 11:31:41 +0000

    closing channel

---
+++ chapter7/listing7.9_10/closing.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func receiver(messages <-chan int) {
+    for {
+        msg := <-messages
+        fmt.Println(time.Now().Format("15:04:05"), "Received:", msg)
+        time.Sleep(1 * time.Second)
+    }
+}
+
+func main() {
+    msgChannel := make(chan int)
+    go receiver(msgChannel)
+    for i := 1; i <= 3; i++ {
+        fmt.Println(time.Now().Format("15:04:05"), "Sending:", i)
+        msgChannel <- i
+        time.Sleep(1 * time.Second)
+    }
+    close(msgChannel)
+    time.Sleep(3 * time.Second)
+}
