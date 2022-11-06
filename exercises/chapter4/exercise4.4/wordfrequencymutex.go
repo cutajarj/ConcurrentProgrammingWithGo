5aeaa14c055bc4bf0270043263431048bdb359fb
James Cutajar <james.cutajar@gmail.com>
2022-11-06 08:31:44 +0000

    exercise 4.4

---
+++ exercises/chapter4/exercise4.4/wordfrequencymutex.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+	"sync"
+	"time"
+)
+
+/*
+Note: this program has a race condition for demonstration purposes
+Additionally we have a timer at the end which you might need to adjust
+depending on how fast your internet connection is.
+In later chapters we cover how to wait for threads to complete their work
+*/
+func countLetters(url string, frequency map[string]int, mutex *sync.Mutex) {
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
+	mutex.Lock()
+	for _, word := range wordRegex.FindAllString(string(body), -1) {
+		wordLower := strings.ToLower(word)
+		frequency[wordLower] += 1
+	}
+	mutex.Unlock()
+	fmt.Println("Completed:", url)
+}
+
+func main() {
+	mutex := sync.Mutex{}
+	var frequency = make(map[string]int)
+	for i := 1000; i <= 1020; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go countLetters(url, frequency, &mutex)
+	}
+	time.Sleep(10 * time.Second)
+	mutex.Lock()
+	for k, v := range frequency {
+		fmt.Println(k, "->", v)
+	}
+	mutex.Unlock()
+}
