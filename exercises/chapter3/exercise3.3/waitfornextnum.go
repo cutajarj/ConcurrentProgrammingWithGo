962fb5916c0fe51a869d5246709fe464fa453809
James Cutajar <james.cutajar@gmail.com>
2022-11-06 07:04:52 +0000

    changing println to use fmt

--- exercises/chapter3/exercise3.3/waitfornextnum.go
+++ exercises/chapter3/exercise3.3/waitfornextnum.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 /*
@@ -10,21 +10,21 @@ import (
 */
 
 func addNextNumber(nextNum *[101]int) {
-    i := 0
-    for nextNum[i] != 0 {
-        i++
-    }
-    nextNum[i] = nextNum[i-1] + 1
+	i := 0
+	for nextNum[i] != 0 {
+		i++
+	}
+	nextNum[i] = nextNum[i-1] + 1
 }
 
 func main() {
-    nextNum := [101]int{1}
-    for i := 0; i < 100; i++ {
-        go addNextNumber(&nextNum)
-    }
-    for nextNum[100] == 0 {
-        println("Waiting for goroutines to complete")
-        time.Sleep(10 * time.Millisecond)
-    }
-    fmt.Println(nextNum)
+	nextNum := [101]int{1}
+	for i := 0; i < 100; i++ {
+		go addNextNumber(&nextNum)
+	}
+	for nextNum[100] == 0 {
+		fmt.Println("Waiting for goroutines to complete")
+		time.Sleep(10 * time.Millisecond)
+	}
+	fmt.Println(nextNum)
 }
