c55befd6f772596619d5a9b4cfdc29ecc5b22466
James Cutajar <james.cutajar@gmail.com>
2022-11-07 08:34:30 +0000

    removing println

--- chapter6/listing6.1/waitforgroup.go
+++ chapter6/listing6.1/waitforgroup.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func doWork(id int, wg *sync.WaitGroup) {
-    i := rand.Intn(5)
-    time.Sleep(time.Duration(i) * time.Second)
-    println(id, "Done working after", i, "seconds")
-    wg.Done()
+	i := rand.Intn(5)
+	time.Sleep(time.Duration(i) * time.Second)
+	fmt.Println(id, "Done working after", i, "seconds")
+	wg.Done()
 }
 
 func main() {
-    wg := sync.WaitGroup{}
-    wg.Add(4)
-    for i := 1; i <= 4; i++ {
-        go doWork(i, &wg)
-    }
-    wg.Wait()
-    println("All complete")
+	wg := sync.WaitGroup{}
+	wg.Add(4)
+	for i := 1; i <= 4; i++ {
+		go doWork(i, &wg)
+	}
+	wg.Wait()
+	fmt.Println("All complete")
 }
