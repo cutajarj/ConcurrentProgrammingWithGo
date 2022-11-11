e175b1db80c0553d7b91278cd2570b88ace63218
James Cutajar <james.cutajar@gmail.com>
2022-11-11 15:43:16 +0000

    removing println

--- chapter6/listing6.4/waitgrpsemamain.go
+++ chapter6/listing6.4/waitgrpsemamain.go
@@ -1,17 +1,20 @@
 package main
 
-import "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter6/listing6.3"
+import (
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter6/listing6.3"
+)
 
 func doWork(id int, wg *listing6_3.WaitGrp) {
-    println(id, "Done working ")
-    wg.Done()
+	fmt.Println(id, "Done working ")
+	wg.Done()
 }
 
 func main() {
-    wg := listing6_3.NewWaitGrp(4)
-    for i := 1; i <= 4; i++ {
-        go doWork(i, wg)
-    }
-    wg.Wait()
-    println("All complete")
+	wg := listing6_3.NewWaitGrp(4)
+	for i := 1; i <= 4; i++ {
+		go doWork(i, wg)
+	}
+	wg.Wait()
+	fmt.Println("All complete")
 }
