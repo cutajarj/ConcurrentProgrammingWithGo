e73b2b9ff0784e72f33a8027f5048155900390a0
James Cutajar <james.cutajar@gmail.com>
2022-11-12 18:17:30 +0000

    removing println

--- chapter5/listing5.18/releasebeforeacquire.go
+++ chapter5/listing5.18/releasebeforeacquire.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter5/listing5.16"
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter5/listing5.16"
 )
 
 func main() {
-    semaphore := listing5_16.NewSemaphore(0)
-    for i := 0; i < 50000; i++ {
-        go doWork(semaphore)
-        println("Waiting for child goroutine")
-        semaphore.Acquire()
-        println("Child goroutine finished")
-    }
+	semaphore := listing5_16.NewSemaphore(0)
+	for i := 0; i < 50000; i++ {
+		go doWork(semaphore)
+		fmt.Println("Waiting for child goroutine")
+		semaphore.Acquire()
+		fmt.Println("Child goroutine finished")
+	}
 }
 
 func doWork(semaphore *listing5_16.Semaphore) {
-    println("Work started")
-    println("Work finished")
-    semaphore.Release()
+	fmt.Println("Work started")
+	fmt.Println("Work finished")
+	semaphore.Release()
 }
-
