9b039406afbd1aa169a6c73f2f260d9ada044f7e
James Cutajar <james.cutajar@gmail.com>
2022-11-15 10:00:51 +0000

    removing println

--- chapter3/listing3.5_6/stingyspendy.go
+++ chapter3/listing3.5_6/stingyspendy.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "time"
+	"fmt"
+	"time"
 )
 
 /*
-  Note: this program has a race condition for demonstration purposes
-  In later chapters we cover how to wait for threads to complete their work
+Note: this program has a race condition for demonstration purposes
+In later chapters we cover how to wait for threads to complete their work
 */
 func stingy(money *int) {
-    for i := 0; i < 1000000; i++ {
-        *money += 10
-    }
-    println("Stingy Done")
+	for i := 0; i < 1000000; i++ {
+		*money += 10
+	}
+	fmt.Println("Stingy Done")
 }
 
 func spendy(money *int) {
-    for i := 0; i < 1000000; i++ {
-        *money -= 10
-    }
-    println("Spendy Done")
+	for i := 0; i < 1000000; i++ {
+		*money -= 10
+	}
+	fmt.Println("Spendy Done")
 }
 
 func main() {
-    money := 100
-    go stingy(&money)
-    go spendy(&money)
-    time.Sleep(2 * time.Second)
-    println("Money in bank account: ", money)
+	money := 100
+	go stingy(&money)
+	go spendy(&money)
+	time.Sleep(2 * time.Second)
+	fmt.Println("Money in bank account: ", money)
 }
