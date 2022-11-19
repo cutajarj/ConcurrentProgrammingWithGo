2d4f2f7bbcfd303003b30295f07dca6ff9f50c43
James Cutajar <james.cutajar@gmail.com>
2022-11-19 21:49:21 +0000

    moving listing

---
+++ chapter6/listing6.12_13/simplebarrierexample.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter6/listing6.10"
+	"time"
+)
+
+func workAndWait(name string, timeToWork int, barrier *listing6_10.Barrier) {
+	start := time.Now()
+	for {
+		fmt.Println(time.Since(start), name, "is running")
+		time.Sleep(time.Duration(timeToWork) * time.Second)
+		fmt.Println(time.Since(start), name, "is waiting on barrier")
+		barrier.Wait()
+	}
+}
+
+func main() {
+	barrier := listing6_10.NewBarrier(2)
+	go workAndWait("Red", 4, barrier)
+	go workAndWait("Blue", 10, barrier)
+	time.Sleep(time.Duration(100) * time.Second)
+}
