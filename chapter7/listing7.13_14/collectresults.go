0ba678190a146a55143e6e58d9c682c22c741312
James Cutajar <james.cutajar@gmail.com>
2022-11-23 19:01:44 +0000

    adding collecting results

---
+++ chapter7/listing7.13_14/collectresults.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findFactors(number int) []int {
+	result := make([]int, 0)
+	for i := 1; i <= number; i++ {
+		if number%i == 0 {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+func main() {
+	resultCh := make(chan []int)
+	go func() {
+		resultCh <- findFactors(3419110721)
+	}()
+	fmt.Println(findFactors(4033836233))
+	fmt.Println(<-resultCh)
+}
