d5f114ba187c7cbcf365404a10aaddbbec9e2267
James Cutajar <james.cutajar@gmail.com>
2022-11-30 11:21:34 +0000

    select example

---
+++ chapter8/listing8.2/nonblocking.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sendMsgAfter(seconds int) <-chan string {
+	messages := make(chan string)
+	go func() {
+		time.Sleep(time.Duration(seconds) * time.Second)
+		messages <- "Hello"
+	}()
+	return messages
+}
+
+func main() {
+	messages := sendMsgAfter(3)
+	for {
+		select {
+		case msg := <-messages:
+			fmt.Println("Message received:", msg)
+			return
+		default:
+			fmt.Println("No messages waiting")
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
