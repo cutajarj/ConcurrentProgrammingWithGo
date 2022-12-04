e47e1c12ebb003dbc33e74ca74ca4398fa689547
James Cutajar <james.cutajar@gmail.com>
2022-12-04 18:26:51 +0000

    changing after 1PR

--- chapter2/listing2.3/parallel.go
+++ chapter2/listing2.3/parallel.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func doWork(id int) {
-    time.Sleep(1 * time.Second)
-    fmt.Println(id, "Work finished")
+	fmt.Println(id, "Work started at", time.Now().Format("15:04:05"))
+	time.Sleep(1 * time.Second)
+	fmt.Println(id, "Work finished at", time.Now().Format("15:04:05"))
 }
 
 func main() {
-    for i := 0; i < 5; i++ {
-        go doWork(i)
-    }
-    time.Sleep(2 * time.Second)
+	for i := 0; i < 5; i++ {
+		go doWork(i)
+	}
+	time.Sleep(2 * time.Second)
 }
