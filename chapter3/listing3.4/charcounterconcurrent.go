f9ffc9cd92802797537dbb5501e1458ff602c16d
James Cutajar <james.cutajar@gmail.com>
2022-12-05 12:27:47 +0000

    changing after 1PR

--- chapter3/listing3.4/charcounterconcurrent.go
+++ chapter3/listing3.4/charcounterconcurrent.go
@@ -1,41 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "strings"
-    "time"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
 /*
-  Note: this program has a race condition for demonstration purposes
-  Additionally we have a timer at the end which you might need to adjust
-  depending on how fast your internet connection is.
-  In later chapters we cover how to wait for threads to complete their work
+Note: this program has a race condition for demonstration purposes
+Additionally we have a timer at the end which you might need to adjust
+depending on how fast your internet connection is.
+In later chapters we cover how to wait for threads to complete their work
 */
 func countLetters(url string, frequency []int) {
-    resp, _ := http.Get(url)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    for _, b := range body {
-        c := strings.ToLower(string(b))
-        cIndex := strings.Index(allLetters, c)
-        if cIndex >= 0 {
-            frequency[cIndex] += 1
-        }
-    }
-    fmt.Println("Completed:", url)
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	for _, b := range body {
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(allLetters, c)
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		}
+	}
+	fmt.Println("Completed:", url)
 }
 
 func main() {
-    var frequency = make([]int, 26)
-    for i := 1000; i <= 1200; i++ {
-        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-        go countLetters(url, frequency)
-    }
-    time.Sleep(10 * time.Second)
-    fmt.Println(frequency)
+	var frequency = make([]int, 26)
+	for i := 1000; i <= 1200; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go countLetters(url, frequency)
+	}
+	time.Sleep(10 * time.Second)
+	for i, c := range "abcdefghijklmnopqrstuvwxyz" {
+		fmt.Printf("%c-%d ", c, frequency[i])
+	}
 }
