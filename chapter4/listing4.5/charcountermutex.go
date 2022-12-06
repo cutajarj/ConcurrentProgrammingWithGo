826bc38990668ee818cc00daf473c79f67336682
James Cutajar <james.cutajar@gmail.com>
2022-12-06 09:50:05 +0000

    changing after 1PR

--- chapter4/listing4.5/charcountermutex.go
+++ chapter4/listing4.5/charcountermutex.go
@@ -1,29 +1,27 @@
 package listing4_5
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "strings"
-    "sync"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
 func CountLetters(url string, frequency []int, mutex *sync.Mutex) {
-    resp, _ := http.Get(url)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    mutex.Lock()
-    for _, b := range body {
-        c := strings.ToLower(string(b))
-        cIndex := strings.Index(allLetters, c)
-        if cIndex >= 0 {
-            frequency[cIndex] += 1
-        }
-    }
-    mutex.Unlock()
-    fmt.Println("Completed:", url)
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	mutex.Lock()
+	for _, b := range body {
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(allLetters, c)
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		}
+	}
+	mutex.Unlock()
+	fmt.Println("Completed:", url)
 }
-
-
