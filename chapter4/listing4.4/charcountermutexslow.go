3ee83ac064a14252772122c71ee71ec7834050eb
James Cutajar <james.cutajar@gmail.com>
2022-12-06 09:49:55 +0000

    changing after 1PR

--- chapter4/listing4.4/charcountermutexslow.go
+++ chapter4/listing4.4/charcountermutexslow.go
@@ -1,31 +1,30 @@
 package listing4_4
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "strings"
-    "sync"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
 // CountLetters
-//Note: this program us locking the entire goroutine with mutex on purpose to demonstrate
-//bad placement of the lock and unlock. We fix this in the next listing
+// Note: this program us locking the entire goroutine with mutex on purpose to demonstrate
+// bad placement of the lock and unlock. We fix this in the next listing
 func CountLetters(url string, frequency []int, mutex *sync.Mutex) {
-    mutex.Lock()
-    resp, _ := http.Get(url)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    for _, b := range body {
-        c := strings.ToLower(string(b))
-        cIndex := strings.Index(allLetters, c)
-        if cIndex >= 0 {
-            frequency[cIndex] += 1
-        }
-    }
-    fmt.Println("Completed:", url)
-    mutex.Unlock()
+	mutex.Lock()
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	for _, b := range body {
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(allLetters, c)
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		}
+	}
+	fmt.Println("Completed:", url)
+	mutex.Unlock()
 }
-
