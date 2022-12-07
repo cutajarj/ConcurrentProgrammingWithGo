633ee5efce166cfcf7821dc92486ff4d9c0436c3
James Cutajar <james.cutajar@gmail.com>
2022-12-07 15:41:44 +0000

    changes after 1pr

--- chapter4/listing4.5main/charcountermutexmain.go
+++ chapter4/listing4.5main/charcountermutexmain.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.5"
-    "sync"
-    "time"
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.5"
+	"sync"
+	"time"
 )
 
 func main() {
-    mutex := sync.Mutex{}
-    var frequency = make([]int, 26)
-    for i := 1000; i <= 1200; i++ {
-        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-        go listing4_5.CountLetters(url, frequency, &mutex)
-    }
-    time.Sleep(10 * time.Second)
-    mutex.Lock()
-    fmt.Println(frequency)
-    mutex.Unlock()
+	mutex := sync.Mutex{}
+	var frequency = make([]int, 26)
+	for i := 1000; i <= 1200; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go listing4_5.CountLetters(url, frequency, &mutex)
+	}
+	time.Sleep(10 * time.Second)
+	mutex.Lock()
+	for i, c := range listing4_5.AllLetters {
+		fmt.Printf("%c-%d ", c, frequency[i])
+	}
+	mutex.Unlock()
 }
