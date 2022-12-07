dca8580ace6192150bf9d3b41296c67bc22c654c
James Cutajar <james.cutajar@gmail.com>
2022-12-07 10:28:07 +0000

    removing printlns

--- chapter4/listing4.12main/readwritemain.go
+++ chapter4/listing4.12main/readwritemain.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
-    "time"
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
+	"time"
 )
 
 func main() {
-    rwMutex := listing4_12.ReadWriteMutex{}
-    for i := 0; i < 10; i++ {
-        go func() {
-            rwMutex.ReadLock()
-            println("Read started")
-            time.Sleep(5 * time.Second)
-            println("Read done")
-            rwMutex.ReadUnlock()
-        }()
-    }
-    time.Sleep(1 * time.Second)
-    println("Write started")
-    rwMutex.WriteLock()
-    println("Write finished")
+	rwMutex := listing4_12.ReadWriteMutex{}
+	for i := 0; i < 10; i++ {
+		go func() {
+			rwMutex.ReadLock()
+			fmt.Println("Read started")
+			time.Sleep(5 * time.Second)
+			fmt.Println("Read done")
+			rwMutex.ReadUnlock()
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Println("Write started")
+	rwMutex.WriteLock()
+	fmt.Println("Write finished")
 }
-
