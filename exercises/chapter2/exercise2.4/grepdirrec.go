bc622ac44abebc112c97213cb48dbffae43126dd
James Cutajar <james.cutajar@gmail.com>
2022-12-12 19:35:47 +0000

    changes after 1pr

--- exercises/chapter2/exercise2.4/grepdirrec.go
+++ exercises/chapter2/exercise2.4/grepdirrec.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    "io/ioutil"
-    "log"
-    "os"
-    "path/filepath"
-    "strings"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 /*
@@ -15,37 +16,37 @@ import (
 */
 
 func grepPath(path string, fileInfo os.FileInfo, searchStr string) {
-    fullPath := filepath.Join(path, fileInfo.Name())
-    if fileInfo.IsDir() {
-        files, err := ioutil.ReadDir(fullPath)
-        if err != nil {
-            log.Fatal(err)
-        }
-        for _, file := range files {
-            go grepPath(fullPath, file, searchStr)
-        }
-    } else {
-        content, err := os.ReadFile(fullPath)
-        if err != nil {
-            log.Fatal(err)
-        }
-        if strings.Contains(string(content), searchStr) {
-            println(fullPath, "contains a match with ", searchStr)
-        } else {
-            println(fullPath, "does NOT contain a match with", searchStr)
-        }
-    }
+	fullPath := filepath.Join(path, fileInfo.Name())
+	if fileInfo.IsDir() {
+		files, err := ioutil.ReadDir(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			go grepPath(fullPath, file, searchStr)
+		}
+	} else {
+		content, err := os.ReadFile(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.Contains(string(content), searchStr) {
+			fmt.Println(fullPath, "contains a match with ", searchStr)
+		} else {
+			fmt.Println(fullPath, "does NOT contain a match with", searchStr)
+		}
+	}
 }
 
 func main() {
-    searchStr := os.Args[1]
-    path := os.Args[2]
-    files, err := ioutil.ReadDir(path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    for _, fileInfo := range files {
-        go grepPath(path, fileInfo, searchStr)
-    }
-    time.Sleep(2 * time.Second)
+	searchStr := os.Args[1]
+	path := os.Args[2]
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, fileInfo := range files {
+		go grepPath(path, fileInfo, searchStr)
+	}
+	time.Sleep(2 * time.Second)
 }
