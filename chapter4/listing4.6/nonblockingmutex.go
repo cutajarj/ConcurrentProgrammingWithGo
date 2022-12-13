75ac94d65e9a19e9550d4d9043e650a8c8284bf6
James Cutajar <james.cutajar@gmail.com>
2022-12-13 10:19:13 +0000

    changes after 1pr

--- chapter4/listing4.6/nonblockingmutex.go
+++ chapter4/listing4.6/nonblockingmutex.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.5"
-    "sync"
-    "time"
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.5"
+	"sync"
+	"time"
 )
 
 func main() {
-    mutex := sync.Mutex{}
-    var frequency = make([]int, 26)
-    for i := 1000; i <= 1200; i++ {
-        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-        go listing4_5.CountLetters(url, frequency, &mutex)
-    }
-    for i := 0; i < 100; i++ {
-        time.Sleep(100 * time.Millisecond)
-        if mutex.TryLock() {
-            fmt.Println(frequency)
-            mutex.Unlock()
-        } else {
-            fmt.Println("Mutex already being used")
-        }
-    }
+	mutex := sync.Mutex{}
+	var frequency = make([]int, 26)
+	for i := 1000; i <= 1200; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go listing4_5.CountLetters(url, frequency, &mutex)
+	}
+	for i := 0; i < 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+		if mutex.TryLock() {
+			for i, c := range listing4_5.AllLetters {
+				fmt.Printf("%c-%d ", c, frequency[i])
+			}
+			fmt.Println()
+			mutex.Unlock()
+		} else {
+			fmt.Println("Mutex already being used")
+		}
+	}
 }
