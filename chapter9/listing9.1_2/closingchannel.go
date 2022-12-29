97827620c5947442ffeeb44c6b5a6c95ed14656d
James Cutajar <james.cutajar@gmail.com>
2022-12-29 08:49:08 +0000

    closing channels

---
+++ chapter9/listing9.1_2/closingchannel.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func printNumbers(numbers <-chan int, quit chan int) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-numbers)
+		}
+		close(quit)
+	}()
+}
+
+func main() {
+	numbers := make(chan int)
+	quit := make(chan int)
+	printNumbers(numbers, quit)
+	next := 0
+	for i := 1; ; i++ {
+		next += i
+		select {
+		case numbers <- next:
+		case <-quit:
+			fmt.Println("Quitting number generation")
+			return
+		}
+	}
+}
