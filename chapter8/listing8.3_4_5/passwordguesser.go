b23b05346998fa6be9d2f671f5ddd42c80e6bff8
James Cutajar <james.cutajar@gmail.com>
2023-01-05 10:59:14 +0000

    chapter 8 password guesser

---
+++ chapter8/listing8.3_4_5/passwordguesser.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	passwordToGuess = "go far"
+	alphabet        = " abcdefghijklmnopqrstuvwxyz"
+)
+
+func toBase27(n int) string {
+	result := ""
+	for n > 0 {
+		result = string(alphabet[n%27]) + result
+		n /= 27
+	}
+	return result
+}
+
+func guessPassword(from int, upto int, stop chan int, result chan string) {
+	for guessN := from; guessN < upto; guessN += 1 {
+		select {
+		case <-stop:
+			fmt.Printf("Stopping at %d [%d,%d)\n", guessN, from, upto)
+			return
+		default:
+			if toBase27(guessN) == passwordToGuess {
+				result <- toBase27(guessN)
+				close(stop)
+				return
+			}
+		}
+	}
+	fmt.Printf("Not found between [%d,%d)\n", from, upto)
+}
+
+func main() {
+	finished := make(chan int)
+	passwordFound := make(chan string)
+	for i := 1; i <= 387420488; i += 10000000 {
+		go guessPassword(i, i+10000000, finished, passwordFound)
+	}
+	fmt.Println("password found:", <-passwordFound)
+	time.Sleep(5 * time.Second)
+}
