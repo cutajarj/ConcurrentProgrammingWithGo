2336c765042ffdbbfc575dc7d233175fd88e6427
James Cutajar <james.cutajar@gmail.com>
2023-01-10 13:54:54 +0000

    chapter 9 fan in pattern

---
+++ chapter9/listing9.10/fanIn.go
@@ -0,0 +1,28 @@
+package listing9_10
+
+import (
+	"sync"
+)
+
+func FanIn[K any](quit <-chan int, allChannels ...<-chan K) chan K {
+	wg := sync.WaitGroup{}
+	wg.Add(len(allChannels))
+	output := make(chan K)
+	for _, c := range allChannels {
+		go func(channel <-chan K) {
+			defer wg.Done()
+			for i := range channel {
+				select {
+				case output <- i:
+				case <-quit:
+					return
+				}
+			}
+		}(c)
+	}
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+	return output
+}
