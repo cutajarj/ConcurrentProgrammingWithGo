8f2e21d87b8f50c47b2a9456e70673807c53ce98
James Cutajar <james.cutajar@gmail.com>
2023-01-15 20:01:27 +0000

    chapter 9 take function

---
+++ chapter9/listing9.18/take.go
@@ -0,0 +1,25 @@
+package listing9_18
+
+func Take[K any](n int, quit chan int, input <-chan K) <-chan K {
+	output := make(chan K)
+	go func() {
+		defer close(output)
+		moreData := true
+		var msg K
+		for n > 0 && moreData {
+			select {
+			case msg, moreData = <-input:
+				if moreData {
+					output <- msg
+					n--
+				}
+			case <-quit:
+				return
+			}
+		}
+		if n == 0 {
+			close(quit)
+		}
+	}()
+	return output
+}
