044391b615d83acfe78f27eaf08c3ef76c287f02
James Cutajar <james.cutajar@gmail.com>
2023-01-19 17:58:41 +0000

    chapter 9 squares until exercise

---
+++ exercises/chapter9/exercise9.5/squaresuntil.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/exercises/chapter9/exercise9.1"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/exercises/chapter9/exercise9.2"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/exercises/chapter9/exercise9.3"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/exercises/chapter9/exercise9.4"
+)
+
+func main() {
+	quit := make(chan int)
+	exercise9_4.Drain(quit,
+		exercise9_3.Print(quit,
+			exercise9_2.TakeUntil(func(s int) bool { return s <= 1000000 }, quit,
+				exercise9_1.GenerateSquares(quit))))
+	<-quit
+}
