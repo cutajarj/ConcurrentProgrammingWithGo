be9ee209989aa78c32b1bd540a11025a911e692d
James Cutajar <james.cutajar@gmail.com>
2023-01-26 08:44:16 +0000

    chapter 10 http server

---
+++ chapter10/listing10.11/httpserver.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
+	"net"
+)
+
+/*
+If you're having problems running this, please make sure that the port 8080 is available
+Note that error handling is omitted so that listing is brief
+*/
+func main() {
+	incomingConnections := make(chan net.Conn)
+	listing10_9.StartHttpWorkers(3, incomingConnections)
+	server, _ := net.Listen("tcp", "localhost:8080")
+	defer server.Close()
+	for {
+		conn, _ := server.Accept()
+		incomingConnections <- conn
+	}
+}
