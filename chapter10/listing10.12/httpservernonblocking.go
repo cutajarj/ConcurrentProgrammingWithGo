103592745845b0ce5e2433462b58a2e7e1a1dd62
James Cutajar <james.cutajar@gmail.com>
2023-01-28 18:16:42 +0000

    chapter 10 http non blocking

---
+++ chapter10/listing10.12/httpservernonblocking.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
+	"net"
+)
+
+/*
+To trigger the "Server is busy" message try running this:
+seq 1 2000 | xargs -Iname  -P100  curl -s "http://localhost:8080/index.html" | grep Busy
+*/
+func main() {
+	incomingConnections := make(chan net.Conn)
+	listing10_9.StartHttpWorkers(3, incomingConnections)
+	server, _ := net.Listen("tcp", "localhost:8080")
+	defer server.Close()
+	for {
+		conn, _ := server.Accept()
+		select {
+		case incomingConnections <- conn:
+		default:
+			fmt.Println("Server is busy")
+			conn.Write([]byte("HTTP/1.1 503 Service Unavailable\r\n\r\n" +
+				"<html>Busy</html>\n"))
+			conn.Close()
+		}
+	}
+}
