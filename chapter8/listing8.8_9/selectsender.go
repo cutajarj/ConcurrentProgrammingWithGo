9b441e942204964ed9a7434174c1b32ba3e6f554
James Cutajar <james.cutajar@gmail.com>
2023-01-28 21:47:52 +0000

    chapter 8 optimizing primes gen

--- chapter8/listing8.8_9/selectsender.go
+++ chapter8/listing8.8_9/selectsender.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"math/rand"
+    "fmt"
+    "math"
+    "math/rand"
 )
 
 func primesOnly(inputs <-chan int) <-chan int {
-	results := make(chan int)
-	go func() {
-		for c := range inputs {
-			isPrime := c != 1
-			for i := 2; i <= int(math.Sqrt(float64(c))); i++ {
-				if c%i == 0 {
-					isPrime = false
-					break
-				}
-			}
-			if isPrime {
-				results <- c
-			}
-		}
-	}()
-	return results
+    results := make(chan int)
+    go func() {
+        for c := range inputs {
+            isPrime := c != 1
+            for i := 2; i <= int(math.Sqrt(float64(c))); i++ {
+                if c%i == 0 {
+                    isPrime = false
+                    break
+                }
+            }
+            if isPrime {
+                results <- c
+            }
+        }
+    }()
+    return results
 }
 
 func main() {
-	isPrimeChannel := make(chan int)
-	primes := primesOnly(isPrimeChannel)
-	for i := 0; i < 100; {
-		select {
-		case isPrimeChannel <- rand.Intn(1000000000) + 1:
-		case p := <-primes:
-			fmt.Println("Found prime:", p)
-			i++
-		}
-	}
+    isPrimeChannel := make(chan int)
+    primes := primesOnly(isPrimeChannel)
+    for i := 0; i < 100; {
+        select {
+        case isPrimeChannel <- rand.Intn(1000000000) + 1:
+        case p := <-primes:
+            fmt.Println("Found prime:", p)
+            i++
+        }
+    }
 }
