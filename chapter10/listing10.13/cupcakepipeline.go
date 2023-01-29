0ce4b9bed384146641d4af971aa25cb3dec6754e
James Cutajar <james.cutajar@gmail.com>
2023-01-29 16:30:33 +0000

    chapter 10 cupcake pipeline

---
+++ chapter10/listing10.13/cupcakepipeline.go
@@ -0,0 +1,43 @@
+package listing10_13
+
+import (
+    "fmt"
+    "time"
+)
+
+const (
+    ovenTime           = 5
+    everyThingElseTime = 2
+)
+
+
+func PrepareTray(trayNumber int) string {
+    fmt.Println("Preparing empty tray", trayNumber)
+    time.Sleep(everyThingElseTime * time.Second)
+    return fmt.Sprintf("tray number %d", trayNumber)
+}
+
+func Mixture(tray string) string {
+    fmt.Println("Pouring cupcake Mixture in", tray)
+    time.Sleep(everyThingElseTime * time.Second)
+    return fmt.Sprintf("cupcake in %s", tray)
+}
+
+func Bake(mixture string) string {
+    fmt.Println("Baking", mixture)
+    time.Sleep(ovenTime * time.Second)
+    return fmt.Sprintf("baked %s", mixture)
+}
+
+func AddToppings(bakedCupCake string) string {
+    fmt.Println("Adding topping to", bakedCupCake)
+    time.Sleep(everyThingElseTime * time.Second)
+    return fmt.Sprintf("topping on %s", bakedCupCake)
+}
+
+func Box(finishedCupCake string) string {
+    fmt.Println("Boxing", finishedCupCake)
+    time.Sleep(everyThingElseTime * time.Second)
+    return fmt.Sprintf("%s boxed", finishedCupCake)
+}
+
