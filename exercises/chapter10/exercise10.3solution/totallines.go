75840ac423a5f4b49de4187a5f9ee2954dd5c717
James Cutajar <james.cutajar@gmail.com>
2023-02-06 12:06:22 +0000

    chapter 10 exercises

---
+++ exercises/chapter10/exercise10.3solution/totallines.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
+)
+
+func main() {
+    const pagesToDownload = 50
+    linesOnPage := make(chan int)
+    finalResult := make(chan int)
+    for i := 1000; i < 1000 + pagesToDownload; i++ {
+        go func(id int) {
+            url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", id)
+            fmt.Println("Downloading", url)
+            resp, _ := http.Get(url)
+            bodyBytes, _ := io.ReadAll(resp.Body)
+            linesOnPage <- strings.Count(string(bodyBytes), "\n")
+            resp.Body.Close()
+        }(i)
+    }
+    go func() {
+        totalLines := 0
+        for i := 0; i < pagesToDownload; i++ {
+            totalLines += <-linesOnPage
+        }
+        finalResult <- totalLines
+    }()
+    fmt.Println("Total lines:", <-finalResult)
+}
+
