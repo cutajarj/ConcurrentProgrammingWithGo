1bc1011275637d61ec5614d3e36d060bbcbb1c5c
James Cutajar <james.cutajar@gmail.com>
2023-02-09 08:47:20 +0000

    reformatting

--- chapter2/listing2.1_2/sequential.go
+++ chapter2/listing2.1_2/sequential.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func doWork(id int) {
-	fmt.Println(id, "Work started at", time.Now().Format("15:04:05"))
-	time.Sleep(1 * time.Second)
-	fmt.Println(id, "Work finished at", time.Now().Format("15:04:05"))
+    fmt.Println(id, "Work started at", time.Now().Format("15:04:05"))
+    time.Sleep(1 * time.Second)
+    fmt.Println(id, "Work finished at", time.Now().Format("15:04:05"))
 }
 
 func main() {
-	for i := 0; i < 5; i++ {
-		doWork(i)
-	}
+    for i := 0; i < 5; i++ {
+        doWork(i)
+    }
 }
--- chapter2/listing2.3/parallel.go
+++ chapter2/listing2.3/parallel.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func doWork(id int) {
-	fmt.Println(id, "Work started at", time.Now().Format("15:04:05"))
-	time.Sleep(1 * time.Second)
-	fmt.Println(id, "Work finished at", time.Now().Format("15:04:05"))
+    fmt.Println(id, "Work started at", time.Now().Format("15:04:05"))
+    time.Sleep(1 * time.Second)
+    fmt.Println(id, "Work finished at", time.Now().Format("15:04:05"))
 }
 
 func main() {
-	for i := 0; i < 5; i++ {
-		go doWork(i)
-	}
-	time.Sleep(2 * time.Second)
+    for i := 0; i < 5; i++ {
+        go doWork(i)
+    }
+    time.Sleep(2 * time.Second)
 }
