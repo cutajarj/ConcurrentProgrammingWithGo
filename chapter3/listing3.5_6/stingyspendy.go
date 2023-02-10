7924b6a7c024e487758bf57b263f0426a8c80100
James Cutajar <james.cutajar@gmail.com>
2023-02-10 20:55:01 +0000

    reformatting

--- chapter3/listing3.5_6/stingyspendy.go
+++ chapter3/listing3.5_6/stingyspendy.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 /*
@@ -10,23 +10,23 @@ Note: this program has a race condition for demonstration purposes
 In later chapters we cover how to wait for threads to complete their work
 */
 func stingy(money *int) {
-	for i := 0; i < 1000000; i++ {
-		*money += 10
-	}
-	fmt.Println("Stingy Done")
+    for i := 0; i < 1000000; i++ {
+        *money += 10
+    }
+    fmt.Println("Stingy Done")
 }
 
 func spendy(money *int) {
-	for i := 0; i < 1000000; i++ {
-		*money -= 10
-	}
-	fmt.Println("Spendy Done")
+    for i := 0; i < 1000000; i++ {
+        *money -= 10
+    }
+    fmt.Println("Spendy Done")
 }
 
 func main() {
-	money := 100
-	go stingy(&money)
-	go spendy(&money)
-	time.Sleep(2 * time.Second)
-	fmt.Println("Money in bank account: ", money)
+    money := 100
+    go stingy(&money)
+    go spendy(&money)
+    time.Sleep(2 * time.Second)
+    fmt.Println("Money in bank account: ", money)
 }
--- chapter3/listing3.7/stingyspendysched.go
+++ chapter3/listing3.7/stingyspendysched.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
-	"time"
+    "fmt"
+    "runtime"
+    "time"
 )
 
 /*
@@ -11,25 +11,25 @@ Note: this program has a race condition for demonstration purposes
 In later chapters we cover how to wait for threads to complete their work
 */
 func stingy(money *int) {
-	for i := 0; i < 1000000; i++ {
-		*money += 10
-		runtime.Gosched()
-	}
-	fmt.Println("Stingy Done")
+    for i := 0; i < 1000000; i++ {
+        *money += 10
+        runtime.Gosched()
+    }
+    fmt.Println("Stingy Done")
 }
 
 func spendy(money *int) {
-	for i := 0; i < 1000000; i++ {
-		*money -= 10
-		runtime.Gosched()
-	}
-	fmt.Println("Spendy Done")
+    for i := 0; i < 1000000; i++ {
+        *money -= 10
+        runtime.Gosched()
+    }
+    fmt.Println("Spendy Done")
 }
 
 func main() {
-	money := 100
-	go stingy(&money)
-	go spendy(&money)
-	time.Sleep(2 * time.Second)
-	fmt.Print("Money in bank account: ", money)
+    money := 100
+    go stingy(&money)
+    go spendy(&money)
+    time.Sleep(2 * time.Second)
+    fmt.Print("Money in bank account: ", money)
 }
