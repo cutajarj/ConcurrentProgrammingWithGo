06a23c2a3bed1b3ff0386c656032c0c990c2bca3
James Cutajar <james.cutajar@gmail.com>
2023-02-10 20:55:14 +0000

    reformatting

--- chapter3/listing3.1/countdown.go
+++ chapter3/listing3.1/countdown.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func countdown(seconds *int) {
-	for *seconds > 0 {
-		time.Sleep(1 * time.Second)
-		*seconds -= 1
-	}
+    for *seconds > 0 {
+        time.Sleep(1 * time.Second)
+        *seconds -= 1
+    }
 }
 
 func main() {
-	count := 5
-	go countdown(&count)
-	for count > 0 {
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println(count)
-	}
+    count := 5
+    go countdown(&count)
+    for count > 0 {
+        time.Sleep(500 * time.Millisecond)
+        fmt.Println(count)
+    }
 }
--- chapter3/listing3.2_3/charcountersequential.go
+++ chapter3/listing3.2_3/charcountersequential.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
 func countLetters(url string, frequency []int) {
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		cIndex := strings.Index(allLetters, c)
-		if cIndex >= 0 {
-			frequency[cIndex] += 1
-		}
-	}
-	fmt.Println("Completed:", url)
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+    body, _ := io.ReadAll(resp.Body)
+    for _, b := range body {
+        c := strings.ToLower(string(b))
+        cIndex := strings.Index(allLetters, c)
+        if cIndex >= 0 {
+            frequency[cIndex] += 1
+        }
+    }
+    fmt.Println("Completed:", url)
 }
 
 func main() {
-	var frequency = make([]int, 26)
-	for i := 1000; i <= 1200; i++ {
-		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		countLetters(url, frequency)
-	}
-	for i, c := range allLetters {
-		fmt.Printf("%c-%d ", c, frequency[i])
-	}
+    var frequency = make([]int, 26)
+    for i := 1000; i <= 1200; i++ {
+        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+        countLetters(url, frequency)
+    }
+    for i, c := range allLetters {
+        fmt.Printf("%c-%d ", c, frequency[i])
+    }
 }
