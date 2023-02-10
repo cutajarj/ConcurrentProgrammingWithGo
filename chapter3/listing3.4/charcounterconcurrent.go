bb40cd557115451090b823364b4f1fd758c22205
James Cutajar <james.cutajar@gmail.com>
2023-02-10 20:55:23 +0000

    reformatting

--- chapter3/listing3.4/charcounterconcurrent.go
+++ chapter3/listing3.4/charcounterconcurrent.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
-	"time"
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
+    "time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
@@ -17,27 +17,27 @@ depending on how fast your internet connection is.
 In later chapters we cover how to wait for threads to complete their work
 */
 func countLetters(url string, frequency []int) {
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		cIndex := strings.Index(allLetters, c)
-		if cIndex >= 0 {
-			frequency[cIndex] += 1
-		}
-	}
-	fmt.Println("Completed:", url)
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+    body, _ := io.ReadAll(resp.Body)
+    for _, b := range body {
+        c := strings.ToLower(string(b))
+        cIndex := strings.Index(allLetters, c)
+        if cIndex >= 0 {
+            frequency[cIndex] += 1
+        }
+    }
+    fmt.Println("Completed:", url)
 }
 
 func main() {
-	var frequency = make([]int, 26)
-	for i := 1000; i <= 1200; i++ {
-		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		go countLetters(url, frequency)
-	}
-	time.Sleep(10 * time.Second)
-	for i, c := range allLetters {
-		fmt.Printf("%c-%d ", c, frequency[i])
-	}
+    var frequency = make([]int, 26)
+    for i := 1000; i <= 1200; i++ {
+        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+        go countLetters(url, frequency)
+    }
+    time.Sleep(10 * time.Second)
+    for i, c := range allLetters {
+        fmt.Printf("%c-%d ", c, frequency[i])
+    }
 }
