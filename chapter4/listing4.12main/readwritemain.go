3a2fab73e0004a6960d137545e8db9bca84068d7
James Cutajar <james.cutajar@gmail.com>
2023-02-13 10:55:10 +0000

    reformatting

--- chapter4/listing4.12main/readwritemain.go
+++ chapter4/listing4.12main/readwritemain.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
-	"time"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
+    "time"
 )
 
 func main() {
-	rwMutex := listing4_12.ReadWriteMutex{}
-	for i := 0; i < 10; i++ {
-		go func() {
-			rwMutex.ReadLock()
-			fmt.Println("Read started")
-			time.Sleep(5 * time.Second)
-			fmt.Println("Read done")
-			rwMutex.ReadUnlock()
-		}()
-	}
-	time.Sleep(1 * time.Second)
-	fmt.Println("Write started")
-	rwMutex.WriteLock()
-	fmt.Println("Write finished")
+    rwMutex := listing4_12.ReadWriteMutex{}
+    for i := 0; i < 10; i++ {
+        go func() {
+            rwMutex.ReadLock()
+            fmt.Println("Read started")
+            time.Sleep(5 * time.Second)
+            fmt.Println("Read done")
+            rwMutex.ReadUnlock()
+        }()
+    }
+    time.Sleep(1 * time.Second)
+    fmt.Println("Write started")
+    rwMutex.WriteLock()
+    fmt.Println("Write finished")
 }
