402ef33f1d1ac17acddff3be8e135e4f91092572
James Cutajar <james.cutajar@gmail.com>
2023-02-13 10:54:50 +0000

    reformatting

--- chapter4/listing4.5/charcountermutex.go
+++ chapter4/listing4.5/charcountermutex.go
@@ -1,28 +1,28 @@
 package listing4_5
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
-	"sync"
-	"time"
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
+    "sync"
+    "time"
 )
 
 const AllLetters = "abcdefghijklmnopqrstuvwxyz"
 
 func CountLetters(url string, frequency []int, mutex *sync.Mutex) {
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	mutex.Lock()
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		cIndex := strings.Index(AllLetters, c)
-		if cIndex >= 0 {
-			frequency[cIndex] += 1
-		}
-	}
-	mutex.Unlock()
-	fmt.Println("Completed:", url, time.Now().Format("15:04:05"))
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+    body, _ := io.ReadAll(resp.Body)
+    mutex.Lock()
+    for _, b := range body {
+        c := strings.ToLower(string(b))
+        cIndex := strings.Index(AllLetters, c)
+        if cIndex >= 0 {
+            frequency[cIndex] += 1
+        }
+    }
+    mutex.Unlock()
+    fmt.Println("Completed:", url, time.Now().Format("15:04:05"))
 }
