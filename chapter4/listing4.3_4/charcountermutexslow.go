f92405944181fe9fab52ae124085b8e9155c5804
James Cutajar <james.cutajar@gmail.com>
2023-02-13 10:55:00 +0000

    reformatting

--- chapter4/listing4.3_4/charcountermutexslow.go
+++ chapter4/listing4.3_4/charcountermutexslow.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"strings"
-	"sync"
-	"time"
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
+    "sync"
+    "time"
 )
 
 const AllLetters = "abcdefghijklmnopqrstuvwxyz"
 
 func main() {
-	mutex := sync.Mutex{}
-	var frequency = make([]int, 26)
-	for i := 1000; i <= 1200; i++ {
-		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		go CountLetters(url, frequency, &mutex)
-	}
-	time.Sleep(100 * time.Second)
-	mutex.Lock()
-	for i, c := range AllLetters {
-		fmt.Printf("%c-%d ", c, frequency[i])
-	}
-	mutex.Unlock()
+    mutex := sync.Mutex{}
+    var frequency = make([]int, 26)
+    for i := 1000; i <= 1200; i++ {
+        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+        go CountLetters(url, frequency, &mutex)
+    }
+    time.Sleep(100 * time.Second)
+    mutex.Lock()
+    for i, c := range AllLetters {
+        fmt.Printf("%c-%d ", c, frequency[i])
+    }
+    mutex.Unlock()
 }
 
 // CountLetters
 // Note: this program us locking the entire goroutine with mutex on purpose to demonstrate
 // bad placement of the lock and unlock. We fix this in the next listing
 func CountLetters(url string, frequency []int, mutex *sync.Mutex) {
-	mutex.Lock()
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		cIndex := strings.Index(AllLetters, c)
-		if cIndex >= 0 {
-			frequency[cIndex] += 1
-		}
-	}
-	fmt.Println("Completed:", url, time.Now().Format("15:04:05"))
-	mutex.Unlock()
+    mutex.Lock()
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+    body, _ := io.ReadAll(resp.Body)
+    for _, b := range body {
+        c := strings.ToLower(string(b))
+        cIndex := strings.Index(AllLetters, c)
+        if cIndex >= 0 {
+            frequency[cIndex] += 1
+        }
+    }
+    fmt.Println("Completed:", url, time.Now().Format("15:04:05"))
+    mutex.Unlock()
 }
