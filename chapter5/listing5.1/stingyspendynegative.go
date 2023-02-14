08531cf48f5cdc1028b75093d73fd6ca9ff474e4
James Cutajar <james.cutajar@gmail.com>
2023-02-14 09:42:47 +0000

    reformatting

--- chapter5/listing5.1/stingyspendynegative.go
+++ chapter5/listing5.1/stingyspendynegative.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"sync"
-	"time"
+    "fmt"
+    "os"
+    "sync"
+    "time"
 )
 
 func stingy(money *int, mutex *sync.Mutex) {
-	for i := 0; i < 1000000; i++ {
-		mutex.Lock()
-		*money += 10
-		mutex.Unlock()
-	}
-	fmt.Println("Stingy Done")
+    for i := 0; i < 1000000; i++ {
+        mutex.Lock()
+        *money += 10
+        mutex.Unlock()
+    }
+    fmt.Println("Stingy Done")
 }
 
 func spendy(money *int, mutex *sync.Mutex) {
-	for i := 0; i < 200000; i++ {
-		mutex.Lock()
-		*money -= 50
-		if *money < 0 {
-			fmt.Println("Money is negative!")
-			os.Exit(1)
-		}
-		mutex.Unlock()
-	}
-	fmt.Println("Spendy Done")
+    for i := 0; i < 200000; i++ {
+        mutex.Lock()
+        *money -= 50
+        if *money < 0 {
+            fmt.Println("Money is negative!")
+            os.Exit(1)
+        }
+        mutex.Unlock()
+    }
+    fmt.Println("Spendy Done")
 }
 
 func main() {
-	money := 100
-	mutex := sync.Mutex{}
-	go stingy(&money, &mutex)
-	go spendy(&money, &mutex)
-	time.Sleep(2 * time.Second)
-	mutex.Lock()
-	fmt.Println("Money in bank account: ", money)
-	mutex.Unlock()
+    money := 100
+    mutex := sync.Mutex{}
+    go stingy(&money, &mutex)
+    go spendy(&money, &mutex)
+    time.Sleep(2 * time.Second)
+    mutex.Lock()
+    fmt.Println("Money in bank account: ", money)
+    mutex.Unlock()
 }
