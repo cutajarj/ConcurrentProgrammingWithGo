91c1877210782398a5497b2fc05e5d9277b66b02
James Cutajar <james.cutajar@gmail.com>
2023-02-14 09:42:31 +0000

    reformatting

--- chapter5/listing5.8_9/gamesync.go
+++ chapter5/listing5.8_9/gamesync.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
+    "fmt"
+    "sync"
+    "time"
 )
 
 func main() {
-	cond := sync.NewCond(&sync.Mutex{})
-	playersInGame := 4
-	for playerId := 0; playerId < 4; playerId++ {
-		go playerHandler(cond, &playersInGame, playerId)
-		time.Sleep(1 * time.Second)
-	}
+    cond := sync.NewCond(&sync.Mutex{})
+    playersInGame := 4
+    for playerId := 0; playerId < 4; playerId++ {
+        go playerHandler(cond, &playersInGame, playerId)
+        time.Sleep(1 * time.Second)
+    }
 }
 
 func playerHandler(cond *sync.Cond, playersRemaining *int, playerId int) {
-	cond.L.Lock()
-	fmt.Println(playerId, ": Connected")
-	*playersRemaining--
-	if *playersRemaining == 0 {
-		cond.Broadcast()
-	}
-	for *playersRemaining > 0 {
-		fmt.Println(playerId, ": Waiting for more players")
-		cond.Wait()
-	}
-	cond.L.Unlock()
-	fmt.Println("All players connected. Ready player", playerId)
-	//Game started
+    cond.L.Lock()
+    fmt.Println(playerId, ": Connected")
+    *playersRemaining--
+    if *playersRemaining == 0 {
+        cond.Broadcast()
+    }
+    for *playersRemaining > 0 {
+        fmt.Println(playerId, ": Waiting for more players")
+        cond.Wait()
+    }
+    cond.L.Unlock()
+    fmt.Println("All players connected. Ready player", playerId)
+    //Game started
 }
