ae2d27f9d134592970c43a2946f535e3b87325ef
James Cutajar <james.cutajar@gmail.com>
2023-02-14 09:42:55 +0000

    reformatting

--- chapter5/listing5.10/writestarvation.go
+++ chapter5/listing5.10/writestarvation.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
-	"time"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter4/listing4.12"
+    "time"
 )
 
 func main() {
-	rwMutex := listing4_12.ReadWriteMutex{}
-	for i := 0; i < 2; i++ {
-		go func() {
-			for {
-				rwMutex.ReadLock()
-				time.Sleep(1 * time.Second)
-				fmt.Println("Read done")
-				rwMutex.ReadUnlock()
-			}
-		}()
-	}
-	time.Sleep(1 * time.Second)
-	rwMutex.WriteLock()
-	fmt.Println("Write finished")
+    rwMutex := listing4_12.ReadWriteMutex{}
+    for i := 0; i < 2; i++ {
+        go func() {
+            for {
+                rwMutex.ReadLock()
+                time.Sleep(1 * time.Second)
+                fmt.Println("Read done")
+                rwMutex.ReadUnlock()
+            }
+        }()
+    }
+    time.Sleep(1 * time.Second)
+    rwMutex.WriteLock()
+    fmt.Println("Write finished")
 }
--- chapter5/listing5.7/waitforonetask.go
+++ chapter5/listing5.7/waitforonetask.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
 func doWork(cond *sync.Cond) {
-	fmt.Println("Work started")
-	fmt.Println("Work finished")
-	cond.L.Lock()
-	cond.Signal()
-	cond.L.Unlock()
+    fmt.Println("Work started")
+    fmt.Println("Work finished")
+    cond.L.Lock()
+    cond.Signal()
+    cond.L.Unlock()
 }
 
 func main() {
-	cond := sync.NewCond(&sync.Mutex{})
-	cond.L.Lock()
-	for i := 0; i < 50000; i++ {
-		go doWork(cond)
-		fmt.Println("Waiting for child goroutine")
-		cond.Wait()
-		fmt.Println("Child goroutine finished")
-	}
-	cond.L.Unlock()
+    cond := sync.NewCond(&sync.Mutex{})
+    cond.L.Lock()
+    for i := 0; i < 50000; i++ {
+        go doWork(cond)
+        fmt.Println("Waiting for child goroutine")
+        cond.Wait()
+        fmt.Println("Child goroutine finished")
+    }
+    cond.L.Unlock()
 }
