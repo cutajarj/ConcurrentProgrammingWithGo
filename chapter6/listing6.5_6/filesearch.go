6a471192e2745e2798a9916785a3aa0b57aaeb72
James Cutajar <james.cutajar@gmail.com>
2023-02-15 11:17:10 +0000

    reformatting

--- chapter6/listing6.5_6/filesearch.go
+++ chapter6/listing6.5_6/filesearch.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
+    "fmt"
+    "os"
+    "path/filepath"
+    "strings"
+    "sync"
 )
 
 func fileSearch(dir string, filename string, wg *sync.WaitGroup) {
-	files, _ := os.ReadDir(dir)
-	for _, file := range files {
-		fpath := filepath.Join(dir, file.Name())
-		if strings.Contains(file.Name(), filename) {
-			fmt.Println(fpath)
-		}
-		if file.IsDir() {
-			wg.Add(1)
-			go fileSearch(fpath, filename, wg)
-		}
-	}
-	wg.Done()
+    files, _ := os.ReadDir(dir)
+    for _, file := range files {
+        fpath := filepath.Join(dir, file.Name())
+        if strings.Contains(file.Name(), filename) {
+            fmt.Println(fpath)
+        }
+        if file.IsDir() {
+            wg.Add(1)
+            go fileSearch(fpath, filename, wg)
+        }
+    }
+    wg.Done()
 }
 
 func main() {
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go fileSearch(os.Args[1], os.Args[2], &wg)
-	wg.Wait()
+    wg := sync.WaitGroup{}
+    wg.Add(1)
+    go fileSearch(os.Args[1], os.Args[2], &wg)
+    wg.Wait()
 }
