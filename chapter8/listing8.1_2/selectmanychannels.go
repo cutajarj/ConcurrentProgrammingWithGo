26bc4e8260ee953b0020d27522295ad29011474f
James Cutajar <james.cutajar@gmail.com>
2023-02-20 11:53:18 +0000

    reformatting

--- chapter8/listing8.1_2/selectmanychannels.go
+++ chapter8/listing8.1_2/selectmanychannels.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func writeEvery(msg string, seconds int) <-chan string {
-	messages := make(chan string)
-	go func() {
-		for {
-			time.Sleep(time.Duration(seconds) * time.Second)
-			messages <- msg
-		}
-	}()
-	return messages
+    messages := make(chan string)
+    go func() {
+        for {
+            time.Sleep(time.Duration(seconds) * time.Second)
+            messages <- msg
+        }
+    }()
+    return messages
 }
 
 func main() {
-	messagesFromA := writeEvery("Tick", 1)
-	messagesFromB := writeEvery("Tock", 3)
-	for {
-		select {
-		case msg1 := <-messagesFromA:
-			fmt.Println(msg1)
-		case msg2 := <-messagesFromB:
-			fmt.Println(msg2)
-		}
-	}
+    messagesFromA := writeEvery("Tick", 1)
+    messagesFromB := writeEvery("Tock", 3)
+    for {
+        select {
+        case msg1 := <-messagesFromA:
+            fmt.Println(msg1)
+        case msg2 := <-messagesFromB:
+            fmt.Println(msg2)
+        }
+    }
 }
