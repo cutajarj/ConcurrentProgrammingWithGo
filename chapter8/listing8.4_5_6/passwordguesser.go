ae092696d398bb34332c3cb6b8775b88cb2b3015
James Cutajar <james.cutajar@gmail.com>
2023-02-20 11:52:39 +0000

    reformatting

--- chapter8/listing8.4_5_6/passwordguesser.go
+++ chapter8/listing8.4_5_6/passwordguesser.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 const (
-	passwordToGuess = "go far"
-	alphabet        = " abcdefghijklmnopqrstuvwxyz"
+    passwordToGuess = "go far"
+    alphabet        = " abcdefghijklmnopqrstuvwxyz"
 )
 
 func toBase27(n int) string {
-	result := ""
-	for n > 0 {
-		result = string(alphabet[n%27]) + result
-		n /= 27
-	}
-	return result
+    result := ""
+    for n > 0 {
+        result = string(alphabet[n%27]) + result
+        n /= 27
+    }
+    return result
 }
 
 func guessPassword(from int, upto int, stop chan int, result chan string) {
-	for guessN := from; guessN < upto; guessN += 1 {
-		select {
-		case <-stop:
-			fmt.Printf("Stopped at %d [%d,%d)\n", guessN, from, upto)
-			return
-		default:
-			if toBase27(guessN) == passwordToGuess {
-				result <- toBase27(guessN)
-				close(stop)
-				return
-			}
-		}
-	}
-	fmt.Printf("Not found between [%d,%d)\n", from, upto)
+    for guessN := from; guessN < upto; guessN += 1 {
+        select {
+        case <-stop:
+            fmt.Printf("Stopped at %d [%d,%d)\n", guessN, from, upto)
+            return
+        default:
+            if toBase27(guessN) == passwordToGuess {
+                result <- toBase27(guessN)
+                close(stop)
+                return
+            }
+        }
+    }
+    fmt.Printf("Not found between [%d,%d)\n", from, upto)
 }
 
 func main() {
-	finished := make(chan int)
-	passwordFound := make(chan string)
-	for i := 1; i <= 387420488; i += 10000000 {
-		go guessPassword(i, i+10000000, finished, passwordFound)
-	}
-	fmt.Println("password found:", <-passwordFound)
-	close(passwordFound)
-	time.Sleep(5 * time.Second)
+    finished := make(chan int)
+    passwordFound := make(chan string)
+    for i := 1; i <= 387420488; i += 10000000 {
+        go guessPassword(i, i+10000000, finished, passwordFound)
+    }
+    fmt.Println("password found:", <-passwordFound)
+    close(passwordFound)
+    time.Sleep(5 * time.Second)
 }
