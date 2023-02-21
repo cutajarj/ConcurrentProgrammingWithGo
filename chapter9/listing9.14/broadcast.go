97572042610a6abea1ae85a8668e2d4c901e1e66
James Cutajar <james.cutajar@gmail.com>
2023-02-21 11:47:47 +0000

    reformatting

--- chapter9/listing9.14/broadcast.go
+++ chapter9/listing9.14/broadcast.go
@@ -1,37 +1,37 @@
 package listing9_14
 
 func Broadcast[K any](quit <-chan int, input <-chan K, n int) []chan K {
-	outputs := CreateAll[K](n)
-	go func() {
-		defer CloseAll(outputs...)
-		var msg K
-		moreData := true
-		for moreData {
-			select {
-			case msg, moreData = <-input:
-				if moreData {
-					for _, output := range outputs {
-						output <- msg
-					}
-				}
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return outputs
+    outputs := CreateAll[K](n)
+    go func() {
+        defer CloseAll(outputs...)
+        var msg K
+        moreData := true
+        for moreData {
+            select {
+            case msg, moreData = <-input:
+                if moreData {
+                    for _, output := range outputs {
+                        output <- msg
+                    }
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return outputs
 }
 
 func CreateAll[K any](n int) []chan K {
-	channels := make([]chan K, n)
-	for i, _ := range channels {
-		channels[i] = make(chan K)
-	}
-	return channels
+    channels := make([]chan K, n)
+    for i, _ := range channels {
+        channels[i] = make(chan K)
+    }
+    return channels
 }
 
 func CloseAll[K any](channels ...chan K) {
-	for _, output := range channels {
-		close(output)
-	}
+    for _, output := range channels {
+        close(output)
+    }
 }
--- chapter9/listing9.5_6/downloadpages.go
+++ chapter9/listing9.5_6/downloadpages.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
+    "fmt"
+    "io"
+    "net/http"
 )
 
 func downloadPages(quit <-chan int, urls <-chan string) <-chan string {
-	pages := make(chan string)
-	go func() {
-		defer close(pages)
-		moreData, url := true, ""
-		for moreData {
-			select {
-			case url, moreData = <-urls:
-				if moreData {
-					resp, _ := http.Get(url)
-					body, _ := io.ReadAll(resp.Body)
-					pages <- string(body)
-					resp.Body.Close()
-				}
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return pages
+    pages := make(chan string)
+    go func() {
+        defer close(pages)
+        moreData, url := true, ""
+        for moreData {
+            select {
+            case url, moreData = <-urls:
+                if moreData {
+                    resp, _ := http.Get(url)
+                    body, _ := io.ReadAll(resp.Body)
+                    pages <- string(body)
+                    resp.Body.Close()
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return pages
 }
 
 func generateUrls(quit <-chan int) <-chan string {
-	urls := make(chan string)
-	go func() {
-		defer close(urls)
-		for i := 100; i <= 150; i++ {
-			url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-			select {
-			case urls <- url:
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return urls
+    urls := make(chan string)
+    go func() {
+        defer close(urls)
+        for i := 100; i <= 150; i++ {
+            url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+            select {
+            case urls <- url:
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return urls
 }
 
 func main() {
-	quit := make(chan int)
-	defer close(quit)
-	results := downloadPages(quit, generateUrls(quit))
-	for result := range results {
-		fmt.Println(result)
-	}
+    quit := make(chan int)
+    defer close(quit)
+    results := downloadPages(quit, generateUrls(quit))
+    for result := range results {
+        fmt.Println(result)
+    }
 }
