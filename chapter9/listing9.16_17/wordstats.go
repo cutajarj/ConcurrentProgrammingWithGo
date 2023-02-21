a3c9edf32896da1170a345d87d717eff297e60d7
James Cutajar <james.cutajar@gmail.com>
2023-02-21 11:48:25 +0000

    reformatting

--- chapter9/listing9.16_17/wordstats.go
+++ chapter9/listing9.16_17/wordstats.go
@@ -1,144 +1,144 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter9/listing9.10"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter9/listing9.14"
-	"io"
-	"net/http"
-	"regexp"
-	"sort"
-	"strings"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter9/listing9.10"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter9/listing9.14"
+    "io"
+    "net/http"
+    "regexp"
+    "sort"
+    "strings"
 )
 
 func frequentWords(quit <-chan int, words <-chan string) <-chan string {
-	mostFrequentWords := make(chan string)
-	go func() {
-		defer close(mostFrequentWords)
-		freqMap := make(map[string]int)
-		freqList := make([]string, 0)
-		moreData, word := true, ""
-		for moreData {
-			select {
-			case word, moreData = <-words:
-				if moreData {
-					if freqMap[word] == 0 {
-						freqList = append(freqList, word)
-					}
-					freqMap[word] += 1
-				}
-			case <-quit:
-				return
-			}
-		}
-		sort.Slice(freqList, func(a, b int) bool {
-			return freqMap[freqList[a]] > freqMap[freqList[b]]
-		})
-		mostFrequentWords <- strings.Join(freqList[:10], ", ")
-	}()
-	return mostFrequentWords
+    mostFrequentWords := make(chan string)
+    go func() {
+        defer close(mostFrequentWords)
+        freqMap := make(map[string]int)
+        freqList := make([]string, 0)
+        moreData, word := true, ""
+        for moreData {
+            select {
+            case word, moreData = <-words:
+                if moreData {
+                    if freqMap[word] == 0 {
+                        freqList = append(freqList, word)
+                    }
+                    freqMap[word] += 1
+                }
+            case <-quit:
+                return
+            }
+        }
+        sort.Slice(freqList, func(a, b int) bool {
+            return freqMap[freqList[a]] > freqMap[freqList[b]]
+        })
+        mostFrequentWords <- strings.Join(freqList[:10], ", ")
+    }()
+    return mostFrequentWords
 }
 
 func longestWords(quit <-chan int, words <-chan string) <-chan string {
-	longWords := make(chan string)
-	go func() {
-		defer close(longWords)
-		uniqueWordsMap := make(map[string]bool)
-		uniqueWords := make([]string, 0)
-		moreData, word := true, ""
-		for moreData {
-			select {
-			case word, moreData = <-words:
-				if moreData && !uniqueWordsMap[word] {
-					uniqueWordsMap[word] = true
-					uniqueWords = append(uniqueWords, word)
-				}
-			case <-quit:
-				return
-			}
-		}
-		sort.Slice(uniqueWords, func(a, b int) bool {
-			return len(uniqueWords[a]) > len(uniqueWords[b])
-		})
-		longWords <- strings.Join(uniqueWords[:10], ", ")
-	}()
-	return longWords
+    longWords := make(chan string)
+    go func() {
+        defer close(longWords)
+        uniqueWordsMap := make(map[string]bool)
+        uniqueWords := make([]string, 0)
+        moreData, word := true, ""
+        for moreData {
+            select {
+            case word, moreData = <-words:
+                if moreData && !uniqueWordsMap[word] {
+                    uniqueWordsMap[word] = true
+                    uniqueWords = append(uniqueWords, word)
+                }
+            case <-quit:
+                return
+            }
+        }
+        sort.Slice(uniqueWords, func(a, b int) bool {
+            return len(uniqueWords[a]) > len(uniqueWords[b])
+        })
+        longWords <- strings.Join(uniqueWords[:10], ", ")
+    }()
+    return longWords
 }
 
 func extractWords(quit <-chan int, pages <-chan string) <-chan string {
-	words := make(chan string)
-	go func() {
-		defer close(words)
-		wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
-		moreData, page := true, ""
-		for moreData {
-			select {
-			case page, moreData = <-pages:
-				if moreData {
-					for _, word := range wordRegex.FindAllString(page, -1) {
-						words <- strings.ToLower(word)
-					}
-				}
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return words
+    words := make(chan string)
+    go func() {
+        defer close(words)
+        wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
+        moreData, page := true, ""
+        for moreData {
+            select {
+            case page, moreData = <-pages:
+                if moreData {
+                    for _, word := range wordRegex.FindAllString(page, -1) {
+                        words <- strings.ToLower(word)
+                    }
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return words
 }
 
 func downloadPages(quit <-chan int, urls <-chan string) <-chan string {
-	pages := make(chan string)
-	go func() {
-		defer close(pages)
-		moreData, url := true, ""
-		for moreData {
-			select {
-			case url, moreData = <-urls:
-				if moreData {
-					resp, _ := http.Get(url)
-					body, _ := io.ReadAll(resp.Body)
-					pages <- string(body)
-					resp.Body.Close()
-				}
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return pages
+    pages := make(chan string)
+    go func() {
+        defer close(pages)
+        moreData, url := true, ""
+        for moreData {
+            select {
+            case url, moreData = <-urls:
+                if moreData {
+                    resp, _ := http.Get(url)
+                    body, _ := io.ReadAll(resp.Body)
+                    pages <- string(body)
+                    resp.Body.Close()
+                }
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return pages
 }
 
 func generateUrls(quit <-chan int) <-chan string {
-	urls := make(chan string)
-	go func() {
-		defer close(urls)
-		for i := 100; i <= 150; i++ {
-			url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-			select {
-			case urls <- url:
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return urls
+    urls := make(chan string)
+    go func() {
+        defer close(urls)
+        for i := 100; i <= 150; i++ {
+            url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+            select {
+            case urls <- url:
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return urls
 }
 
 const downloaders = 20
 
 func main() {
-	quit := make(chan int)
-	defer close(quit)
-	urls := generateUrls(quit)
-	pages := make([]<-chan string, downloaders)
-	for i := 0; i < downloaders; i++ {
-		pages[i] = downloadPages(quit, urls)
-	}
-	words := extractWords(quit, listing9_10.FanIn(quit, pages...))
-	wordsMulti := listing9_14.Broadcast(quit, words, 2)
-	longestResults := longestWords(quit, wordsMulti[0])
-	frequentResults := frequentWords(quit, wordsMulti[1])
-	fmt.Println("Longest Words:", <-longestResults)
-	fmt.Println("Most frequent Words:", <-frequentResults)
+    quit := make(chan int)
+    defer close(quit)
+    urls := generateUrls(quit)
+    pages := make([]<-chan string, downloaders)
+    for i := 0; i < downloaders; i++ {
+        pages[i] = downloadPages(quit, urls)
+    }
+    words := extractWords(quit, listing9_10.FanIn(quit, pages...))
+    wordsMulti := listing9_14.Broadcast(quit, words, 2)
+    longestResults := longestWords(quit, wordsMulti[0])
+    frequentResults := frequentWords(quit, wordsMulti[1])
+    fmt.Println("Longest Words:", <-longestResults)
+    fmt.Println("Most frequent Words:", <-frequentResults)
 }
--- chapter9/listing9.3_4/generateurls.go
+++ chapter9/listing9.3_4/generateurls.go
@@ -3,26 +3,26 @@ package main
 import "fmt"
 
 func generateUrls(quit <-chan int) <-chan string {
-	urls := make(chan string)
-	go func() {
-		defer close(urls)
-		for i := 100; i <= 150; i++ {
-			url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-			select {
-			case urls <- url:
-			case <-quit:
-				return
-			}
-		}
-	}()
-	return urls
+    urls := make(chan string)
+    go func() {
+        defer close(urls)
+        for i := 100; i <= 150; i++ {
+            url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+            select {
+            case urls <- url:
+            case <-quit:
+                return
+            }
+        }
+    }()
+    return urls
 }
 
 func main() {
-	quit := make(chan int)
-	defer close(quit)
-	results := generateUrls(quit)
-	for result := range results {
-		fmt.Println(result)
-	}
+    quit := make(chan int)
+    defer close(quit)
+    results := generateUrls(quit)
+    for result := range results {
+        fmt.Println(result)
+    }
 }
