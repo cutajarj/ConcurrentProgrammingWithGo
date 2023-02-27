162f3b673deae52dddca7317542e14c5ccf659ab
James Cutajar <james.cutajar@gmail.com>
2023-02-27 12:46:56 +0000

    reformatting

--- chapter10/listing10.2/dirfilehash.go
+++ chapter10/listing10.2/dirfilehash.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.1"
-	"os"
-	"path/filepath"
-	"sync"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.1"
+    "os"
+    "path/filepath"
+    "sync"
 )
 
 func main() {
-	dir := os.Args[1]
-	files, _ := os.ReadDir(dir)
-	wg := sync.WaitGroup{}
-	for _, file := range files {
-		if !file.IsDir() {
-			wg.Add(1)
-			go func(filename string) {
-				fPath := filepath.Join(dir, filename)
-				hash := listing10_1.FHash(fPath)
-				fmt.Printf("%s - %x\n", filename, hash)
-				wg.Done()
-			}(file.Name())
-		}
-	}
-	wg.Wait()
+    dir := os.Args[1]
+    files, _ := os.ReadDir(dir)
+    wg := sync.WaitGroup{}
+    for _, file := range files {
+        if !file.IsDir() {
+            wg.Add(1)
+            go func(filename string) {
+                fPath := filepath.Join(dir, filename)
+                hash := listing10_1.FHash(fPath)
+                fmt.Printf("%s - %x\n", filename, hash)
+                wg.Done()
+            }(file.Name())
+        }
+    }
+    wg.Wait()
 }
--- chapter10/listing10.4/dirhashconcurrent.go
+++ chapter10/listing10.4/dirhashconcurrent.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"crypto/md5"
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.1"
-	"os"
-	"path/filepath"
+    "crypto/md5"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.1"
+    "os"
+    "path/filepath"
 )
 
 func main() {
-	dir := os.Args[1]
-	files, _ := os.ReadDir(dir)
-	hMd5 := md5.New()
-	var prev, next chan int
-	for _, file := range files {
-		if !file.IsDir() {
-			next = make(chan int)
-			go func(filename string, prev, next chan int) {
-				fpath := filepath.Join(dir, filename)
-				hashOnFile := listing10_1.FHash(fpath)
-				if prev != nil {
-					<-prev
-				}
-				hMd5.Write(hashOnFile)
-				next <- 0
-			}(file.Name(), prev, next)
-			prev = next
-		}
-	}
-	<-next
-	fmt.Printf("%s - %x\n", dir, hMd5.Sum(nil))
+    dir := os.Args[1]
+    files, _ := os.ReadDir(dir)
+    hMd5 := md5.New()
+    var prev, next chan int
+    for _, file := range files {
+        if !file.IsDir() {
+            next = make(chan int)
+            go func(filename string, prev, next chan int) {
+                fpath := filepath.Join(dir, filename)
+                hashOnFile := listing10_1.FHash(fpath)
+                if prev != nil {
+                    <-prev
+                }
+                hMd5.Write(hashOnFile)
+                next <- 0
+            }(file.Name(), prev, next)
+            prev = next
+        }
+    }
+    <-next
+    fmt.Printf("%s - %x\n", dir, hMd5.Sum(nil))
 }
