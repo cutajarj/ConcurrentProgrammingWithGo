f0a16ebe3e0c25d4d3a5bfdfc12f4444cc1a6e4c
James Cutajar <james.cutajar@gmail.com>
2023-02-28 10:00:33 +0000

    reformatting

--- chapter10/listing10.1/filehash.go
+++ chapter10/listing10.1/filehash.go
@@ -1,17 +1,17 @@
 package listing10_1
 
 import (
-	"crypto/md5"
-	"io"
-	"os"
+    "crypto/md5"
+    "io"
+    "os"
 )
 
 func FHash(filepath string) []byte {
-	file, _ := os.Open(filepath)
-	defer file.Close()
+    file, _ := os.Open(filepath)
+    defer file.Close()
 
-	hMd5 := md5.New()
-	io.Copy(hMd5, file)
+    hMd5 := md5.New()
+    io.Copy(hMd5, file)
 
-	return hMd5.Sum(nil)
+    return hMd5.Sum(nil)
 }
