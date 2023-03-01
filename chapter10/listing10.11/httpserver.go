f1da15d10db63bab845828f9d9962d24adc2a0ef
James Cutajar <james.cutajar@gmail.com>
2023-03-01 09:00:41 +0000

    reformatting

--- chapter10/listing10.11/httpserver.go
+++ chapter10/listing10.11/httpserver.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
-	"net"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
+    "net"
 )
 
 /*
@@ -10,12 +10,12 @@ If you're having problems running this, please make sure that the port 8080 is a
 Note that error handling is omitted so that listing is brief
 */
 func main() {
-	incomingConnections := make(chan net.Conn)
-	listing10_9.StartHttpWorkers(3, incomingConnections)
-	server, _ := net.Listen("tcp", "localhost:8080")
-	defer server.Close()
-	for {
-		conn, _ := server.Accept()
-		incomingConnections <- conn
-	}
+    incomingConnections := make(chan net.Conn)
+    listing10_9.StartHttpWorkers(3, incomingConnections)
+    server, _ := net.Listen("tcp", "localhost:8080")
+    defer server.Close()
+    for {
+        conn, _ := server.Accept()
+        incomingConnections <- conn
+    }
 }
--- chapter10/listing10.12/httpservernonblocking.go
+++ chapter10/listing10.12/httpservernonblocking.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
-	"net"
+    "fmt"
+    "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.9"
+    "net"
 )
 
 /*
@@ -11,19 +11,19 @@ To trigger the "Server is busy" message try running this:
 seq 1 2000 | xargs -Iname  -P100  curl -s "http://localhost:8080/index.html" | grep Busy
 */
 func main() {
-	incomingConnections := make(chan net.Conn)
-	listing10_9.StartHttpWorkers(3, incomingConnections)
-	server, _ := net.Listen("tcp", "localhost:8080")
-	defer server.Close()
-	for {
-		conn, _ := server.Accept()
-		select {
-		case incomingConnections <- conn:
-		default:
-			fmt.Println("Server is busy")
-			conn.Write([]byte("HTTP/1.1 503 Service Unavailable\r\n\r\n" +
-				"<html>Busy</html>\n"))
-			conn.Close()
-		}
-	}
+    incomingConnections := make(chan net.Conn)
+    listing10_9.StartHttpWorkers(3, incomingConnections)
+    server, _ := net.Listen("tcp", "localhost:8080")
+    defer server.Close()
+    for {
+        conn, _ := server.Accept()
+        select {
+        case incomingConnections <- conn:
+        default:
+            fmt.Println("Server is busy")
+            conn.Write([]byte("HTTP/1.1 503 Service Unavailable\r\n\r\n" +
+                "<html>Busy</html>\n"))
+            conn.Close()
+        }
+    }
 }
