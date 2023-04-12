52837430cf393b7e9977f74692c89f643e5c1574
James Cutajar <james.cutajar@gmail.com>
2023-04-12 09:38:32 +0100

    atomic letter counter

---
+++ chapter12/listing12.4_5/atomiccharcounter.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "net/http"
+    "strings"
+    "sync"
+    "sync/atomic"
+)
+
+const allLetters = "abcdefghijklmnopqrstuvwxyz"
+
+func countLetters(url string, frequency []int32) {
+    resp, _ := http.Get(url)
+    defer resp.Body.Close()
+    body, _ := io.ReadAll(resp.Body)
+    for _, b := range body {
+        c := strings.ToLower(string(b))
+        cIndex := strings.Index(allLetters, c)
+        if cIndex >= 0 {
+            atomic.AddInt32(&frequency[cIndex], 1)
+        }
+    }
+    fmt.Println("Completed:", url)
+}
+
+
+func main() {
+    wg := sync.WaitGroup{}
+    wg.Add(201)
+    var frequency = make([]int32, 26)
+    for i := 1000; i <= 1200; i++ {
+        url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+        go func() {
+            countLetters(url, frequency)
+            wg.Done()
+        }()
+    }
+    wg.Wait()
+    for i, c := range allLetters {
+        fmt.Printf("%c-%d ", c, atomic.LoadInt32(&frequency[i]))
+    }
+}
+
