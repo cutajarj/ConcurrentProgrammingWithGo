2bb404b05ca229d843472211f5acba6e5edfdf64
James Cutajar <james.cutajar@gmail.com>
2023-04-17 15:18:20 +0100

    spin locks simple implementation

---
+++ chapter12/listing12.9/spinlocks.go
@@ -0,0 +1,24 @@
+package listing12_9
+
+import (
+    "runtime"
+    "sync"
+    "sync/atomic"
+)
+
+type SpinLock int32
+
+func (s *SpinLock) Lock() {
+    for !atomic.CompareAndSwapInt32((*int32)(s), 0, 1) {
+        runtime.Gosched()
+    }
+}
+
+func (s *SpinLock) Unlock() {
+    atomic.StoreInt32((*int32)(s), 0)
+}
+
+func NewSpinLock() sync.Locker {
+    var lock SpinLock
+    return &lock
+}
