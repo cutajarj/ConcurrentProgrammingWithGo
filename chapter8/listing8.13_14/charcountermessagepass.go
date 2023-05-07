22c8fb381116fddf692b00b791248e2527b9bc6d
James Cutajar <james.cutajar@gmail.com>
2023-05-07 19:39:59 +0100

    changing implementation to download smaller number of pages

--- chapter8/listing8.13_14/charcountermessagepass.go
+++ chapter8/listing8.13_14/charcountermessagepass.go
@@ -16,6 +16,9 @@ func countLetters(url string) <-chan []int {
         frequency := make([]int, 26)
         resp, _ := http.Get(url)
         defer resp.Body.Close()
+        if resp.StatusCode != 200 {
+            panic("Server returning error code: " + resp.Status)
+        }
         body, _ := io.ReadAll(resp.Body)
         for _, b := range body {
             c := strings.ToLower(string(b))
@@ -33,7 +36,7 @@ func countLetters(url string) <-chan []int {
 func main() {
     results := make([]<-chan []int, 0)
     totalFrequencies := make([]int, 26)
-    for i := 1000; i <= 1200; i++ {
+    for i := 1000; i <= 1030; i++ {
         url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
         results = append(results, countLetters(url))
     }
