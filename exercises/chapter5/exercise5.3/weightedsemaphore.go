ed87d5a64c8c77fb64253db773c266c62fbf3367
James Cutajar <james.cutajar@gmail.com>
2023-05-17 19:59:22 +0100

    Fixed exercise 5.3 solution

--- exercises/chapter5/exercise5.3/weightedsemaphore.go
+++ exercises/chapter5/exercise5.3/weightedsemaphore.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
 type WeightedSemaphore struct {
@@ -18,20 +20,32 @@ func NewSemaphore(n int) *WeightedSemaphore {
 
 func (rw *WeightedSemaphore) Acquire(permits int) {
 	rw.cond.L.Lock()
-	for rw.permits <= 0 {
+	for rw.permits - permits < 0 {
 		rw.cond.Wait()
 	}
-	rw.permits--
+	rw.permits-=permits
 	rw.cond.L.Unlock()
 }
 
 func (rw *WeightedSemaphore) Release(permits int) {
 	rw.cond.L.Lock()
-	rw.permits++
+	rw.permits+=permits
 	rw.cond.Signal()
 	rw.cond.L.Unlock()
 }
 
 func main() {
-
+	sema := NewSemaphore(3)
+	sema.Acquire(2)
+	fmt.Println("Parent thread acquired semaphore")
+	go func() {
+		sema.Acquire(2)
+		fmt.Println("Child thread acquired semaphore")
+		sema.Release(2)
+		fmt.Println("Child thread released semaphore")
+	}()
+	time.Sleep(3 * time.Second)
+	fmt.Println("Parent thread releasing semaphore")
+	sema.Release(2)
+	time.Sleep(1 * time.Second)
 }
