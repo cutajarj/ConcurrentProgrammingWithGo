a475aaae8d0ad39f2671f93d8fb110da61d243de
James Cutajar <james.cutajar@gmail.com>
2023-06-09 17:46:01 +0100

    Adding comment

--- chapter2/listing2.5/goscheduler.go
+++ chapter2/listing2.5/goscheduler.go
@@ -4,7 +4,10 @@ import (
     "fmt"
     "runtime"
 )
-
+/*
+Note: this program is an example of what not to do; using go scheduler
+to synchronize executions
+*/
 func sayHello() {
     fmt.Println("Hello")
 }
