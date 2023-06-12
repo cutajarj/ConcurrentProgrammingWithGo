a9b2638e18502db03dda99805f425c93d0676433
James Cutajar <james.cutajar@gmail.com>
2023-06-12 17:14:12 +0100

    Changing the linked list to a slice

--- chapter4/listing4.7_8_9/matchmonitor.go
+++ chapter4/listing4.7_8_9/matchmonitor.go
@@ -1,53 +1,52 @@
 package main
 
 import (
-    "container/list"
     "fmt"
     "strconv"
     "sync"
     "time"
 )
 
-func matchRecorder(matchEvents *list.List, mutex *sync.Mutex) {
+func matchRecorder(matchEvents *[]string, mutex *sync.Mutex) {
     for i := 0; ; i++ {
         mutex.Lock()
-        matchEvents.PushBack("Match event " + strconv.Itoa(i))
+        *matchEvents = append(*matchEvents,
+            "Match event " + strconv.Itoa(i))
         mutex.Unlock()
         time.Sleep(200 * time.Millisecond)
         fmt.Println("Appended match event")
     }
 }
 
-func clientHandler(mEvents *list.List, mutex *sync.Mutex, st time.Time) {
+func clientHandler(mEvents *[]string, mutex *sync.Mutex, st time.Time) {
     for i := 0; i < 100; i ++ {
         mutex.Lock()
         allEvents := copyAllEvents(mEvents)
         mutex.Unlock()
+
         timeTaken := time.Since(st)
         fmt.Println(len(allEvents), "events copied in", timeTaken)
     }
 }
 
-func copyAllEvents(matchEvents *list.List) []string {
-    i := 0
-    allEvents := make([]string, matchEvents.Len())
-    for e := matchEvents.Front(); e != nil; e = e.Next() {
-        allEvents[i] = e.Value.(string)
-        i++
+func copyAllEvents(matchEvents *[]string) []string {
+    allEvents := make([]string, 0, len(*matchEvents))
+    for _, e := range *matchEvents {
+        allEvents = append(allEvents, e)
     }
     return allEvents
 }
 
 func main() {
     mutex := sync.Mutex{}
-    var matchEvents = list.New()
+    var matchEvents = make([]string, 0, 10000)
     for j := 0; j < 10000; j++ {
-        matchEvents.PushBack("Match event")
+        matchEvents = append(matchEvents, "Match event")
     }
-    go matchRecorder(matchEvents, &mutex)
+    go matchRecorder(&matchEvents, &mutex)
     start := time.Now()
     for j := 0; j < 5000; j++ {
-        go clientHandler(matchEvents, &mutex, start)
+        go clientHandler(&matchEvents, &mutex, start)
     }
     time.Sleep(100 * time.Second)
 }
