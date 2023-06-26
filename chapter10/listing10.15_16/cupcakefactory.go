409b7dfd578413aff3e1e612b92ab70c9c183352
James Cutajar <james.cutajar@gmail.com>
2023-06-26 07:56:24 +0100

    Fixing listing so that the quit channel is first argument

--- chapter10/listing10.15_16/cupcakefactory.go
+++ chapter10/listing10.15_16/cupcakefactory.go
@@ -5,7 +5,7 @@ import (
     "github.com/cutajarj/ConcurrentProgrammingWithGo/chapter10/listing10.13"
 )
 
-func AddOnPipe[X, Y any](f func(X) Y, q <-chan int, in <-chan X) chan Y {
+func AddOnPipe[X, Y any](q <-chan int, f func(X) Y, in <-chan X) chan Y {
     output := make(chan Y)
     go func() {
         defer close(output)
@@ -24,11 +24,11 @@ func AddOnPipe[X, Y any](f func(X) Y, q <-chan int, in <-chan X) chan Y {
 func main() {
     input := make(chan int)
     quit := make(chan int)
-    output := AddOnPipe(listing10_13.Box, quit,
-        AddOnPipe(listing10_13.AddToppings, quit,
-            AddOnPipe(listing10_13.Bake, quit,
-                AddOnPipe(listing10_13.Mixture, quit,
-                    AddOnPipe(listing10_13.PrepareTray, quit, input)))))
+    output := AddOnPipe(quit, listing10_13.Box,
+        AddOnPipe(quit, listing10_13.AddToppings,
+            AddOnPipe(quit, listing10_13.Bake,
+                AddOnPipe(quit, listing10_13.Mixture,
+                    AddOnPipe(quit, listing10_13.PrepareTray, input)))))
     go func() {
         for i := 0; i < 10; i++ {
             input <- i
