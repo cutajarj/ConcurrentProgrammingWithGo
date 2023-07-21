9576ca7dd0b653ebcf0cc00c4dee6b12b573fe73
James Cutajar <james.cutajar@gmail.com>
2023-07-21 08:46:58 +0100

    exercise 4.1

--- exercises/chapter4/exercise4.1/countdown.go
+++ exercises/chapter4/exercise4.1/countdown.go
@@ -2,32 +2,22 @@ package main
 
 import (
     "fmt"
-    "sync"
     "time"
 )
 
-func countdown(seconds *int, mutex *sync.Mutex) {
-    mutex.Lock()
-    remaining := *seconds
-    mutex.Unlock()
-    for remaining > 0 {
+func countdown(seconds *int) {
+    for *seconds > 0 {
         time.Sleep(1 * time.Second)
-        mutex.Lock()
         *seconds -= 1
-        remaining = *seconds
-        mutex.Unlock()
     }
 }
 
 func main() {
-    mutex := sync.Mutex{}
     count := 5
-    go countdown(&count, &mutex)
+    go countdown(&count)
     for count > 0 {
         time.Sleep(500 * time.Millisecond)
-        mutex.Lock()
         fmt.Println(count)
-        mutex.Unlock()
     }
 }
 
