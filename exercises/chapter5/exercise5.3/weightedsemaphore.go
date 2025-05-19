2a17dc3cf0547bb320f30c8bbdac9736cba0bf3c
cutajarj <james.cutajar@gmail.com>
2025-05-19 09:14:19 +0100

    Fixing bug on exercise 5.3. Release should be calling broadcast instead of signal.
    Scenario:
    Goroutine A is holding 2 permits. Goroutines B and C are each waiting to acquire 1 permit.
    
    Later A finishes and releases the 2 permits. Both B and C should be able to acquire a permit immediately.

--- exercises/chapter5/exercise5.3/weightedsemaphore.go
+++ exercises/chapter5/exercise5.3/weightedsemaphore.go
@@ -20,32 +20,40 @@ func NewSemaphore(n int) *WeightedSemaphore {
 
 func (rw *WeightedSemaphore) Acquire(permits int) {
 	rw.cond.L.Lock()
-	for rw.permits - permits < 0 {
+	for rw.permits-permits < 0 {
 		rw.cond.Wait()
 	}
-	rw.permits-=permits
+	rw.permits -= permits
 	rw.cond.L.Unlock()
 }
 
 func (rw *WeightedSemaphore) Release(permits int) {
 	rw.cond.L.Lock()
-	rw.permits+=permits
-	rw.cond.Signal()
+	rw.permits += permits
+	rw.cond.Broadcast()
 	rw.cond.L.Unlock()
 }
 
 func main() {
-	sema := NewSemaphore(3)
+	sema := NewSemaphore(2)
 	sema.Acquire(2)
-	fmt.Println("Parent thread acquired semaphore")
+	fmt.Println("Parent thread A acquired semaphore")
 	go func() {
-		sema.Acquire(2)
-		fmt.Println("Child thread acquired semaphore")
-		sema.Release(2)
-		fmt.Println("Child thread released semaphore")
+		sema.Acquire(1)
+		fmt.Println("Child B thread acquired semaphore")
+		time.Sleep(1 * time.Second)
+		sema.Release(1)
+		fmt.Println("Child B thread released semaphore")
+	}()
+	go func() {
+		sema.Acquire(1)
+		fmt.Println("Child C thread acquired semaphore")
+		time.Sleep(1 * time.Second)
+		sema.Release(1)
+		fmt.Println("Child C thread released semaphore")
 	}()
 	time.Sleep(3 * time.Second)
-	fmt.Println("Parent thread releasing semaphore")
+	fmt.Println("Parent thread A releasing semaphore")
 	sema.Release(2)
-	time.Sleep(1 * time.Second)
+	time.Sleep(3 * time.Second)
 }
